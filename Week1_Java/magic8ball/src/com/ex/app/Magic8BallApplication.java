1ad2aba1494bb89787ffebde1352d965a41ee44e
rjefferson <reginald.jefferson@outlook.com>
2020-04-07 16:22:16 -0400

    Messages updated, others added

--- Week1_Java/magic8ball/src/com/ex/app/Magic8BallApplication.java
+++ Week1_Java/magic8ball/src/com/ex/app/Magic8BallApplication.java
@@ -6,7 +6,26 @@ public class Magic8BallApplication extends Application {
 
     private Magic8Ball magic8Ball;
     private String title;
-    private String[] message = {"Nah", "This ain't it chief", "Big Yes", "Maybe", "Rethink that", "Absolutely"};
+    private String[] message = {"As I see it, yes.\n",
+            " Ask again later.\n",
+            " Better not tell you now.\n",
+            " Cannot predict now.\n" +
+            " Concentrate and ask again.\n",
+            " Don’t count on it.\n",
+            " It is certain.\n",
+            " It is decidedly so.\n",
+            " Most likely.\n",
+            " My reply is no.\n",
+            " My sources say no.\n",
+            " Outlook not so good.\n",
+            " Outlook good.\n",
+            " Reply hazy, try again.\n",
+            " Signs point to yes.\n",
+            " Very doubtful.\n",
+            " Without a doubt.\n",
+            " Yes.\n",
+            " Yes – definitely.\n",
+            " You may rely on it.\n"};
 
     public Magic8BallApplication(){
         magic8Ball = new Magic8Ball(0, message.length);

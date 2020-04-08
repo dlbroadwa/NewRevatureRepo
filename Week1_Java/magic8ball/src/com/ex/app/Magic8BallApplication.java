e0967a63eb28f8954e3c97065e645cacbbc8215c
rjefferson <reginald.jefferson@outlook.com>
2020-04-08 17:00:32 -0400

    Changed to array list and made more dynamic with less hard coding

--- Week1_Java/magic8ball/src/com/ex/app/Magic8BallApplication.java
+++ Week1_Java/magic8ball/src/com/ex/app/Magic8BallApplication.java
@@ -20,17 +20,18 @@ public class  Magic8BallApplication extends Application {
   private Scanner scanner;
   private Screen currentScreen = null;
   private String currentQuestion = "";
-  private AnswerService answerService = null;
+  private AnswerService answerService = new AnswerService("resources/answers");
 
   public Magic8BallApplication() {
-    magic8Ball = new Magic8Ball(5, 0);
+    int answerSize = answerService.answers.size()-1;
+    magic8Ball = new Magic8Ball(answerSize, 0);
     this.scanner = new Scanner(System.in); // set our scanner to read input from the user
     currentScreen = new QuestionScreen();
-    answerService = new AnswerService("resources/answers");
+    //answerService = new AnswerService("resources/answers");
   }
 
   public Magic8BallApplication(String title) {
-    // call the no-args constructor to setup the magic8ball without repeating code
+    // call the no-args constructor to set up the magic8ball without repeating code
     this();
     this.title = title;
   }
@@ -80,3 +81,5 @@ public class  Magic8BallApplication extends Application {
     return answerService;
   }
 }
+
+
--- Week1_Java/magic8ball/src/com/ex/services/AnswerService.java
+++ Week1_Java/magic8ball/src/com/ex/services/AnswerService.java
@@ -1,14 +1,19 @@
 package com.ex.services;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AnswerService {
-  String[] answers = null;
+  //String[] answers = null;
+  public ArrayList<String> answers = new ArrayList<>();
+
 
   public AnswerService(String answerFilePath) {
-    FileReader reader = null;
-    BufferedReader bReader = null;
-    answers = new String[100];
+    FileReader reader;
+    BufferedReader bReader;
+    //answers = new String[100];
+    //List<String> answers = new ArrayList<String>();
 
     try {
       reader = new FileReader(answerFilePath); // this reads a file character-by-character
@@ -17,13 +22,15 @@ public class AnswerService {
       String line = "";
       int index = 0;
       while((line = bReader.readLine()) != null) { // read each line until EOF
-        answers[index++] = line; // add the new line to the answers array
+        answers.add(index++, line); // add the new line to the answers array
       }
 
 
     } catch (FileNotFoundException e) {
       System.err.println("Error loading answer file, using backup");
-      answers = new String[]{"Ask again later"};
+      answers = new ArrayList<String>() {};
+      answers.add("Ask again later");//(ArrayList<String>) Arrays.asList(new String[]{"Ask again later"});
+
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -31,6 +38,8 @@ public class AnswerService {
 
   public String getAnswer(int index) {
     // make sure to handle index bounds
-    return answers[index];
+    return answers.get(index);
   }
+
+
 }

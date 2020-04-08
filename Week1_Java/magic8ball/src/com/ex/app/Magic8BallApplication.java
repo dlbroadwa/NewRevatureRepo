b71776273627120db143256e4971e8beacaa31ce
Nathan Nguyen <nathannguyen@gmail.com>
2020-04-08 15:28:46 -0400

    please work 2

--- Week1_Java/magic8ball/src/com/ex/app/Magic8BallApplication.java
+++ Week1_Java/magic8ball/src/com/ex/app/Magic8BallApplication.java
@@ -1,8 +1,6 @@
 package com.ex.app;
 
 
-<<<<<<< HEAD
-=======
 import com.ex.screens.QuestionScreen;
 import com.ex.screens.Screen;
 
@@ -10,17 +8,12 @@ import javax.sound.midi.Soundbank;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
->>>>>>> 69a2117bfa2b7bf2561564eb40a86ca3eadeae83
+
 // Concrete class
 // must impl Application
 public class Magic8BallApplication extends Application {
 
-<<<<<<< HEAD
-  private  Magic8Ball magic8Ball;
 
-  public Magic8BallApplication() {
-    magic8Ball = new Magic8Ball(3, 0);
-=======
   // Implement an AnswerScreen
   // that receives the question from the question screen and generate and displays and answer
 
@@ -32,7 +25,7 @@ public class Magic8BallApplication extends Application {
     magic8Ball = new Magic8Ball(3, 0);
     this.scanner = new Scanner(System.in); // set our scanner to read input from the user
     currentScreen = new QuestionScreen();
->>>>>>> 69a2117bfa2b7bf2561564eb40a86ca3eadeae83
+
   }
 
   public Magic8BallApplication(String title) {
@@ -43,15 +36,7 @@ public class Magic8BallApplication extends Application {
 
   @Override
   public void run() {
-<<<<<<< HEAD
-    String[] strArray= new String[4];
-    strArray[0] = "Hello 1";
-    strArray[1] = "Hello 2";
-    strArray[2] = "Hello 3";
-    strArray[3] = "Hello 4";
-    System.out.println(strArray[magic8Ball.shake()]);
 
-=======
 
 
       while(currentScreen != null) {
@@ -81,6 +66,5 @@ public class Magic8BallApplication extends Application {
 
   public Scanner getScanner() {
     return scanner;
->>>>>>> 69a2117bfa2b7bf2561564eb40a86ca3eadeae83
   }
 }

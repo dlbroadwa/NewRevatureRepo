7a7dcb098d08dec7f204382d1ef50b9047c17e1c
dylanchhinn <dylanchhinn@gmail.com>
2020-04-08 11:02:34 -0700

    Added 8 ball response

--- Week1_Java/magic8ball/src/com/ex/screens/AnswerScreen.java
+++ Week1_Java/magic8ball/src/com/ex/screens/AnswerScreen.java
@@ -1,6 +1,7 @@
 package com.ex.screens;
 
 import com.ex.app.Application;
+import com.ex.app.Magic8Ball;
 import com.ex.app.Magic8BallApplication;
 
 import java.util.Scanner;
@@ -9,6 +10,9 @@ public class AnswerScreen implements Screen {
 
     String answer;
 
+    private String [] responses = {"No", "Yes", "Maybe"};
+
+    Magic8Ball magic8Ball = new Magic8Ball(responses.length - 1, 0);
     public AnswerScreen (String answer)
     {
         this.answer = answer;
@@ -17,6 +21,7 @@ public class AnswerScreen implements Screen {
     @Override
     public Screen doScreen(Application app) {
         System.out.println("You asked the 8 Ball " + getAnswer());
+        System.out.println(responses[magic8Ball.shake()]);
         return null;
     }
 

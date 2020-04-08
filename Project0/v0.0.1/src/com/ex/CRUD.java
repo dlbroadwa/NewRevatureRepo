af0c87485e1db847fad6428e92180c45bd955e0b
Christopher Hill <chrimhill@gmail.com>
2020-04-07 22:07:50 -0500

    most basic implementation of Read

--- Project0/v0.0.1/src/com/ex/CRUD.java
+++ Project0/v0.0.1/src/com/ex/CRUD.java
@@ -2,7 +2,7 @@ package com.ex;
 
 abstract public class CRUD {
     abstract public void create(TestEntity testEntity, String filePath);
-//    abstract public void read(TestEntity testEntity, String filePath);
+    abstract public TestEntity read(String filePath);
 //    abstract public void update(TestEntity testEntity, String filePath);
 //    abstract public void delete(TestEntity testEntity, String filePath);
 }
--- Project0/v0.0.1/src/com/ex/JavaFileIO.java
+++ Project0/v0.0.1/src/com/ex/JavaFileIO.java
@@ -6,8 +6,8 @@ public class JavaFileIO extends CRUD{
     @Override
     public void create(TestEntity testEntity, String filePath) {
         try {
-            java.io.FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath));
-            java.io.ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(testEntity);
             objectOutputStream.close();
             fileOutputStream.close();
@@ -17,10 +17,21 @@ public class JavaFileIO extends CRUD{
         }
     }
 
-//    @Override
-//    public void read(TestEntity testEntity, String filePath) {
-//
-//    }
+    @Override
+    public TestEntity read(String filePath) {
+        try{
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            TestEntity testEntity = (TestEntity)objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+            System.out.println(testEntity.toString() + " was read from file.");
+            return testEntity;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 //
 //    @Override
 //    public void update(TestEntity testEntity, String filePath) {

cdd335c9f3e4e8225be5b678bf7a908261e0ffe7
Christopher Hill <chrimhill@gmail.com>
2020-04-08 11:55:50 -0500

    initial version of ConsoleInput

--- Project0/v0.0.1/src/com/ex/ConsoleInput.java
+++ Project0/v0.0.1/src/com/ex/ConsoleInput.java
@@ -1,5 +1,34 @@
 package com.ex;
 
-public class ConsoleInput {
+import java.io.Console;
 
+public class ConsoleInput {
+    static private java.util.Scanner scanner;
+
+    static public void initialize(){
+        if(scanner == null){
+            scanner = new java.util.Scanner(System.in);
+        }
+    }
+
+    static public double getNumber(){
+        boolean inputNeeded = true;
+        double num = 0;
+        System.out.println("Please enter a number.");
+        do{
+            try{
+                num = scanner.nextDouble();
+                return num;
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("That wasn't a number. Please enter just a number.");
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            scanner.nextLine();
+        } while(inputNeeded);
+        return num;
+    }
+
+    private ConsoleInput(){
+    }
 }
--- Project0/v0.0.1/src/com/ex/Main.java
+++ Project0/v0.0.1/src/com/ex/Main.java
@@ -6,14 +6,15 @@ public class Main {
         //read from file and create localMemoryArray of Entity objects (contains all file entries)
 
         //read a number in from ConsoleInput
+        ConsoleInput consoleInput;
+        ConsoleInput.initialize();
+        double number = ConsoleInput.getNumber();
+        System.out.println("Success! You entered: " + number + ".");
 
         //check to see if number exists in localMemoryArray of Entity objects
 
         //attempt to serialize new number into permanent storage
 
         //testing
-        TestEntity testEntity = new TestEntity(20);
-        CRUD crud = new JavaFileIO();
-        crud.create(testEntity, "nowhere");
     }
 }

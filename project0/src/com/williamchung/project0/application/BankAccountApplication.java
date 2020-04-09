7f4c46f1f8d00d72de5dcfc4f98ed71381be29a1
will <will0700@gmail.com>
2020-04-08 23:02:37 -0700

    create screens, start mysql connection implementation

--- project0/src/com/williamchung/project0/application/BankAccountApplication.java
+++ project0/src/com/williamchung/project0/application/BankAccountApplication.java
@@ -1,24 +1,48 @@
 package com.williamchung.project0.application;
 
+import com.williamchung.project0.screens.LoginScreen;
 import com.williamchung.project0.screens.Screen;
 
+import java.sql.*;
 import java.util.Scanner;
 
 public class BankAccountApplication extends Application{
-
+    private Connection connection;
     private Scanner scanner;
     private Screen screen = null;
 
-    public BankAccountApplication(){}
-    public BankAccountApplication(String title){
-        this();
-        this.title = title;
-    }
+    //Models to create? Am I biting off too much?
+//    private User user = null;
 
+    //Constructors
+    public BankAccountApplication(String title){
+        this.title = title;
+        this.scanner = new Scanner(System.in);
+        screen = new LoginScreen();
+    }
+    public BankAccountApplication(){
+        this("BankAccountApplication");
+    }
 
     @Override
     public void run() {
-        System.out.println("Bank Application is running");
+        System.out.println("Bank Application is starting");
+
+        //MySQL connection (will separate to configurations package later)
+        //Should I be doing this in Main, with Connection in the application constructor?
+        String mySQLurl = "jdbc:mysql://localhost:3306";
+        String mySQLuser = "root"; //Hey August
+        String mySQLpass = "root"; //You'll probably need to tweak these lines
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(mySQLurl, mySQLuser, mySQLpass);
+            System.out.println("MySQL connected successfully");
+        } catch(Exception exception){
+            System.out.println("MySQL failed to connect");
+            System.out.println(exception);
+        }
+
+        //
         while(screen != null) {
             screen = screen.doScreen(this);
         }
--- project0/src/com/williamchung/project0/screens/LoginScreen.java
+++ project0/src/com/williamchung/project0/screens/LoginScreen.java
@@ -1,10 +1,53 @@
 package com.williamchung.project0.screens;
 
 import com.williamchung.project0.application.Application;
+import com.williamchung.project0.application.BankAccountApplication;
+import com.williamchung.project0.services.LoginService;
+
+import java.sql.Connection;
+import java.util.Scanner;
 
 public class LoginScreen implements Screen {
+
+    private LoginService loginService = new LoginService();
+
+    //This is for when I fix MySQL connection
+//    private final Connection connection;
+//
+//    public LoginScreen(Connection connection) {
+//        this.connection = connection;
+//    }
+
     @Override
     public Screen doScreen(Application app) {
+        Scanner scanner = ((BankAccountApplication) app).getScanner();
+        int threeLoginAttempts = 0;
+        while(threeLoginAttempts < 3){
+            //Take input with Scanner
+            System.out.println("Please enter Username");
+            String username = scanner.nextLine();
+            if(username.equals("\\q")){
+                break;
+            }
+            System.out.println("Please enter 4-digit PIN");
+            String pin = scanner.nextLine();
+            if(pin.equals("\\q")) {
+                break;
+            }
+
+            //Authenticate user with LoginService
+            if(loginService.authenticateUser(username, pin)){ //Hardwired true for now
+
+//                (BankAccountApplication) app.setCurrentUser(user);
+                return new HomeScreen();
+            } else {
+                threeLoginAttempts++;
+                System.out.println("Login Invalid. Please try again. Attempt #" + threeLoginAttempts);
+                continue;
+                //Do not display whether Username or PIN was invalid
+            }
+        }
+        System.out.println("Too many unsuccessful attempts. Please try again later.");
         return null;
     }
 }

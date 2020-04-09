782f163c23d008be8676f79d1a0d1092234c9e66
Danarrius Broadway <dlbroadwa@gmail.com>
2020-04-09 08:57:26 -0500

    started project

--- Project_Zero/src/com/company/app/Candy.java
+++ Project_Zero/src/com/company/app/Candy.java
@@ -1,4 +1,8 @@
 package com.company.app;
 
 public class Candy {
+    private String name;
+    private int units;
+    private float purchaseCost;
+    private float salesCost;
 }
--- Project_Zero/src/com/company/app/User.java
+++
@@ -1,4 +0,0 @@
-package com.company.app;
-
-public class User {
-}

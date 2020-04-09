d63a91a01458986e6df80943586cfc6f4c63efae
johnny.sheerin.is <johnny.sheerin.is@gmail.com>
2020-04-09 05:44:41 -0700

    work please

---
+++ Week1_Java/magic8ball/src/com/ex/app/OptimisticMagic8BallApplication.java
@@ -0,0 +1,8 @@
+package com.ex.app;
+
+public class OptimisticMagic8BallApplication extends Application{
+  @Override
+  public void run() {
+    System.out.println("All signs point to yes");
+  }
+}

34eb4fcde65a7cb972784d90b44cf6c6c653cef5
will <will0700@gmail.com>
2020-04-08 23:02:37 -0700

    create screens, start mysql connection implementation

---
+++ project0/src/com/williamchung/project0/screens/TransferScreen.java
@@ -0,0 +1,10 @@
+package com.williamchung.project0.screens;
+
+import com.williamchung.project0.application.Application;
+
+public class TransferScreen implements Screen {
+    @Override
+    public Screen doScreen(Application app) {
+        return null;
+    }
+}

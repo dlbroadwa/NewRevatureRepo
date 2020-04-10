3918082d2838bf8300f5f922d88043f2e1a9d387
dylanchhinn <dylanchhinn@gmail.com>
2020-04-10 12:27:40 -0700

    Add unit test

---
+++ Project0/src/test/java/test/BankTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Test;
+
+public class BankTest {
+
+    @Test // this is a single test
+    public void shouldPrintHelloWorld() {
+        System.out.println("Hello, World");
+    }
+}

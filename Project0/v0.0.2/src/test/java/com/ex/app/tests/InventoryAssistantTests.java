1fcab1705ae9f2783f3962860dd6d15a53ba665c
Christopher Hill <chrimhill@gmail.com>
2020-04-13 07:51:06 -0500

    Initial commit with JUnit canary tests passing

---
+++ Project0/v0.0.2/src/test/java/com/ex/app/tests/InventoryAssistantTests.java
@@ -0,0 +1,11 @@
+package com.ex.app.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InventoryAssistantTests {
+    @Test
+    public void canaryFalse(){ Assert.assertFalse(false);}
+    @Test
+    public void canaryTrue(){ Assert.assertTrue(true); }
+}

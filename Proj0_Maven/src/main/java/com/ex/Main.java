4694b6df18c46a405ef5393c8185aa5c1b587382
Perry Lee <perrylee_2004@yahoo.com>
2020-04-17 17:50:58 -0400

    Move database credentials to environment variables

--- Proj0_Maven/src/main/java/com/ex/Main.java
+++ Proj0_Maven/src/main/java/com/ex/Main.java
@@ -11,8 +11,11 @@ public class Main {
 
     public static void main(String[] args) {
         String url = "jdbc:postgresql://database-1.c7zjtw5vhjwr.us-east-2.rds.amazonaws.com:5432/postgres";
-        String username = "library_admin";
-        String password = "my$ecurep@ssw0rd";
+        String username = System.getenv("LIBRARY_ADMIN_USER");
+        String password = System.getenv("LIBRARY_ADMIN_PASSWORD");
+        //System.out.printf("user = %s, password = %s%n", username, password);
+        /*String username = "library_admin";
+        String password = "my$ecurep@ssw0rd";*/
         DatabaseConnection connection = new PostgreSQLConnection(url, username, password, "project0");
 
         if (!connection.isDriverInitialized()) {

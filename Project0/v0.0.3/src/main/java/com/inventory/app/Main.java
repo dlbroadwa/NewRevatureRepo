2c326026ecf786dff25f4f4715aa8ecb50534655
Christopher Hill <chrimhill@gmail.com>
2020-04-18 11:01:27 -0500

    add simple examples to Main.java

--- Project0/v0.0.3/src/main/java/com/inventory/app/Main.java
+++ Project0/v0.0.3/src/main/java/com/inventory/app/Main.java
@@ -2,8 +2,10 @@ package com.inventory.app;
 
 import com.inventory.controller.services.connect.PostgresSQLService;
 import com.inventory.controller.services.data.ItemCRUD;
+import com.inventory.controller.services.data.WarehouseCRUD;
 import com.inventory.controller.system.ConsoleOut;
 import com.inventory.model.Item;
+import com.inventory.model.Warehouse;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,7 +15,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         //establish connection
-        try{
+        try {
             PostgresSQLService.addDBConnection("jdbc:postgresql://dbinstance1.c2b26c4tx3es.us-east-2.rds.amazonaws.com:5432/db1",
                     "davide", "jw8s0F4");
 
@@ -27,7 +29,7 @@ public class Main {
         //ItemCRUD tests
         System.out.println("Beginning ItemCRUD examples");
         ItemCRUD itemCRUD = new ItemCRUD();
-        List<Item> testList = null;
+        List testList = null;
 
         System.out.println("");
         System.out.println("read and print the current list");
@@ -36,14 +38,14 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
+        for (Object i : testList)
             ConsoleOut.println(i.toString());
 
         System.out.println("");
         System.out.println("add a new test obj");
         Item testItem = new Item(44, "test", 4234.23, (short) 34);
         try {
-            itemCRUD.create(0,testItem);
+            itemCRUD.create(0, testItem);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -55,12 +57,12 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
-            ConsoleOut.println(i.toSQLString());
+        for (Object i : testList)
+            ConsoleOut.println(((Item) i).toSQLString());
 
         System.out.println("");
         System.out.println("update the value of the test obj");
-        Item secondTestItem = new Item(20, "testtest", 20.00, (short)20);
+        Item secondTestItem = new Item(20, "testtest", 20.00, (short) 20);
         try {
             itemCRUD.update(0, testItem, secondTestItem);
         } catch (SQLException e) {
@@ -74,12 +76,12 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
-            ConsoleOut.println(i.toSQLString());
+        for (Object i : testList)
+            ConsoleOut.println(((Item) i).toSQLString());
 
         System.out.println("");
         System.out.println("delete the updated test obj");
-        try{
+        try {
             itemCRUD.delete(0, secondTestItem);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -92,29 +94,31 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
-            ConsoleOut.println(i.toSQLString());
+        for (Object i : testList)
+            ConsoleOut.println(((Item) i).toSQLString());
 
         //WarehouseCRUD tests
+        System.out.println("");
+        System.out.println("-------------------------------");
         System.out.println("Beginning WarehouseCRUD examples");
-        ItemCRUD warehouseCRUD = new ItemCRUD();
-        List<Item> warehouseList = null;
+        WarehouseCRUD warehouseCRUD = new WarehouseCRUD();
+        testList = null;
 
         System.out.println("");
         System.out.println("read and print the current list");
         try {
-            testList = itemCRUD.read(0);
+            testList = warehouseCRUD.read(0);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
+        for (Object i : testList)
             ConsoleOut.println(i.toString());
 
         System.out.println("");
         System.out.println("add a new test obj");
-        Item testItem = new Item(44, "test", 4234.23, (short) 34);
+        Warehouse testWarehouse1 = new Warehouse(444, "TX", "Nowhere", "5234 Nowhere ln", 99999);
         try {
-            itemCRUD.create(0,testItem);
+            warehouseCRUD.create(0, testWarehouse1);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -122,18 +126,18 @@ public class Main {
         System.out.println("");
         System.out.println("read and print the added obj list");
         try {
-            testList = itemCRUD.read(0);
+            testList = warehouseCRUD.read(0);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
-            ConsoleOut.println(i.toSQLString());
+        for (Object i : testList)
+            ConsoleOut.println(((Warehouse) i).toSQLString());
 
         System.out.println("");
         System.out.println("update the value of the test obj");
-        Item secondTestItem = new Item(20, "testtest", 20.00, (short)20);
+        Warehouse testWarehouse2 = new Warehouse(000, "FL", "Nowhere!", "9934 Nowhere ln", 00000);
         try {
-            itemCRUD.update(0, testItem, secondTestItem);
+            warehouseCRUD.update(0, testWarehouse1, testWarehouse2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -141,17 +145,17 @@ public class Main {
         System.out.println("");
         System.out.println("read and print the updated obj list");
         try {
-            testList = itemCRUD.read(0);
+            testList = warehouseCRUD.read(0);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
-            ConsoleOut.println(i.toSQLString());
+        for (Object i : testList)
+            ConsoleOut.println(((Warehouse) i).toSQLString());
 
         System.out.println("");
         System.out.println("delete the updated test obj");
-        try{
-            itemCRUD.delete(0, secondTestItem);
+        try {
+            warehouseCRUD.delete(0, testWarehouse2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -159,11 +163,11 @@ public class Main {
         System.out.println("");
         System.out.println("read and print the deleted obj, obj list");
         try {
-            testList = itemCRUD.read(0);
+            testList = warehouseCRUD.read(0);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (Item i: testList)
-            ConsoleOut.println(i.toSQLString());
+        for (Object i : testList)
+            ConsoleOut.println(((Warehouse) i).toSQLString());
     }
 }

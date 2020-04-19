d58372ff53dd0d6bc76901b95ffa6929b349bd0c
Paityn Maynard <paityn.maynard@gmail.com>
2020-04-18 21:27:37 -0400

    Update Project_0

--- myTables.sql
+++ myTables.sql
@@ -39,32 +39,8 @@ select * from project_0.animal_inventory order by type;
 
 delete from project_0.animal_inventory where name='Gally' and type='Alligator';
 
--- You should know how to alter a table
--- alter table
-
--- insert DML
--- be sure to use single quotes for values
---insert into public.users (password , email) values ('0123456789012345678901234567890123456789','paityn.maynard@gmail.com');
---insert into public.users (password , email) values
---('0123456789012345678901234567890123456789','janed@gmail.com'),
---('0123456789012345678901234567890123456789','barv@gmail.com'),
---('0123456789012345678901234567890123456789','pad@gmail.com');
-
---update/delete DML
---updates typically have a where clause
---updates w/o where clause will attempt to change the data for all rows
---update public.users set email = 'paityn.maynard@gmail.com' where id=1 and email = 'paitynmaynard@gmail.com';
-
--- deletes typically have a where clause
---deletes w/o a where clause will delete every row from the table
---delete from public.users where email like 'paityn.%@%' ;
-
---select DQL
---select * from public.users;
---select email as username, "password" as secreat from public.users;
-
-
-
-
-
-
+create table if not exists project_0.tansactions(
+	username VARCHAR(20) not null,
+	action VARCHAR(20) not null,
+	cur_timestamp TIMESTAMP(8)
+);

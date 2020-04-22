b6178d14d658ed268717ed5d14fdf67e83b6081c
jpragasa <jpragasa@seu.edu>
2020-04-22 17:33:32 -0400

    TODO get js script to work

---
+++ Week3_ClientSideTech/classcode/js_dom/index.js
@@ -0,0 +1,93 @@
+class TodoItem {
+    // JS classes all have a special constructor method named "constructor"
+    // it is used like constructors in other languages
+    // JS classes can extend other classes
+    //if your JS class extends another class your must call super() in your constructor
+    constructor(id, name) {
+      this._id = id;
+      this._name = name;
+    }
+
+    // there are no private modifiers in JS. An _ is generally used to denote a property that should be
+    // treated as a private member
+    // use get and set keywords to create getter and setter properties to manipulate "private" memebers
+
+    get id() {return this._id;}
+
+    set id(id) {this._id = id;}
+
+    get name() {return this._name;}
+
+    set name(name) {this._name = name;}
+  }
+
+  class TodoList 
+  {
+    constructor(id, name, todos) {
+      this._name = name;
+      this._id = id;
+      this._todos = todos || [];
+    }
+
+    get name() {return this._name;}
+
+    set name(name) {this._name = name;}
+
+    get todos() {return this._todos;}
+
+    set todos(todos) {this._todos = todos;}
+
+    get id() {return this._id;}
+
+    set id(id) {this._id = id;}
+  }
+
+  // Create a way to input into a new list
+  
+
+  function addToList()
+  {
+    var item = new TodoList(2, 'Josh' [new TodoItem(2, "Hello Josh")]);
+    todoLists.push(item);
+  }
+  
+  const todoLists = 
+  [
+    new TodoList(1, 'Karate Kid', [new TodoItem(1, 'Paint the fence')]),
+    //new TodoList(id, name, [new TodoItem(num2, greet)]),
+  ];
+  function renderList() {
+    currentList.todos.forEach((t) => 
+    {
+      const span = document.createElement('span');
+      span.innerHTML = t.name;
+      document.querySelector('#current-list-target').innerHTML = '';
+      document.querySelector('#current-list-target').appendChild(span);
+    });
+  }
+  
+  let currentList = null;
+  
+  // loop over all the todo lists adding eachone to the aside list for displaying
+  todoLists.forEach((e) => 
+  {
+    const li = document.createElement('li'); // create a disconnected element
+    li.innerHTML = e.name;
+    li.setAttribute('data-id', e.id);
+
+    // add a click listener to the li for more interactivity
+    // this will set the current list to displayed on the other section of the page
+    li.addEventListener('click', (e) => 
+    {
+      const targetId = parseInt(e.target.getAttribute('data-id'));
+      currentList = todoLists.find((l) => l.id === targetId); // high order function
+      renderList();
+    });
+    document.querySelector('#lists-list').appendChild(li); // connect the element to the tree as a child of ul#lists-list
+  });
+
+  if (currentList === null) 
+  {
+    document.querySelector('#current-list-target').innerHTML =
+      'No list to display';
+  }

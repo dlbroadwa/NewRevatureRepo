46389e8f01cc8abacb6d0ccc3f9d5f1601995479
AustinKind <austin.t.kind@gmail.com>
2020-04-22 10:14:10 -0500

    clean up

--- Project 0/src/ticket/screen/UserAccessScreen.java
+++ Project 0/src/ticket/screen/UserAccessScreen.java
@@ -46,8 +46,8 @@ public class UserAccessScreen implements Screen {
 		System.out.println("////////////////////////////////////////////////////////////////////////////////////////////////////////");
 		System.out.println("//                                            YOUR TICKETS                                            //");
 		System.out.println("////////////////////////////////////////////////////////////////////////////////////////////////////////");
-		System.out.println(" #     User ID                          Title                      Status   Priority     Last Updated");
-		System.out.println("--- --------------  ---------------------------------------------  ------   --------  -------------------");
+		System.out.println(" #     User ID                          Title                     Status    Priority     Last Updated");
+		System.out.println("--- --------------  --------------------------------------------- ------    --------  -------------------");
 		if (tickets.size() == 0) {
 			System.out.println("\n                                         You have no tickets.");
 		} else {
@@ -66,13 +66,13 @@ public class UserAccessScreen implements Screen {
 					title = "(No Title)";
 				if (title.length() > 45)
 					title = title.substring(0, 42) + "...";
-				result += title + "  ";
+				result += title + " ";
 				int titleSpace = 45 - title.length();
 				result += getWhitespace(titleSpace);
 				result += "(" + ticket.getStatus() + ")";
 				int statusSpace = 6 - ticket.getStatus().length();
 				result += getWhitespace(statusSpace);
-				result += "(" + ticket.getPriority() + ")  ";
+				result += " (" + ticket.getPriority() + ")  ";
 				int prioritySpace = 6 - ticket.getPriority().length();
 				result += getWhitespace(prioritySpace);
 				result += ticket.getLastUpdated().format(dateFormat);	

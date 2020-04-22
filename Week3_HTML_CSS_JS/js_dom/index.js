095fe3969a0ab95c72952317cbd946a018a2d32f
johnny.sheerin.is <johnny.sheerin.is@gmail.com>
2020-04-22 14:56:20 -0700

    js example working list sort of...

---
+++ Week3_HTML_CSS_JS/js_dom/index.js
@@ -0,0 +1,109 @@
+class TodoItem {
+    // JS classes all have a special constructor method named "constructor"
+    // it is used like constructors in other languages
+    // JS classes can extend other classes
+    //if your JS class extends another class your must call super() in your constructor
+    constructor(id, name) {
+      this._id = id;
+      this._name = name;
+    }
+
+    // there are no private modifiers in JS. An _ is generally used to denote a property that should be
+    // treated as a private member
+    // use get and set keywords to create getter and setter properties to manipulate "private" memebers
+
+    get id() {
+      return this._id;
+    }
+
+    set id(id) {
+      this._id = id;
+    }
+
+    get name() {
+      return this._name;
+    }
+
+    set name(name) {
+      this._name = name;
+    }
+  }
+
+  class TodoList {
+    constructor(id, name, todos) {
+      this._name = name;
+      this._id = id;
+      this._todos = todos || [];
+    }
+
+    get name() {
+      return this._name;
+    }
+
+    set name(name) {
+      this._name = name;
+    }
+
+    get todos() {
+      return this._todos;
+    }
+
+    set todos(todos) {
+      this._todos = todos;
+    }
+
+    get id() {
+      return this._id;
+    }
+
+    set id(id) {
+      this._id = id;
+    }
+  }
+
+  const todoLists = [
+    new TodoList(1, 'Karate Kid', [new TodoItem(1, 'Paint the fence')]),
+    new TodoList(1, 'Bob the builder', [new TodoItem(2, 'Build')]),
+  ];
+
+  function renderList() {
+    currentList.todos.forEach((t) => {
+      const span = document.createElement('span');
+      span.innerHTML = t.name;
+      document.querySelector('#current-list-target').innerHTML = '';
+      document.querySelector('#current-list-target').appendChild(span);
+    });
+  }
+
+  let currentList = null;
+
+  // loop over all the todo lists adding eachone to the aside list for displaying
+  todoLists.forEach((e) => {
+    const li = document.createElement('li'); // create a disconnected element
+    li.innerHTML = e.name;
+    li.setAttribute('data-id', e.id);
+
+    // add a click listener to the li for more interactivity
+    // this will set the current list to displayed on the other section of the page
+    li.addEventListener('click', (e) => {
+      const targetId = parseInt(e.target.getAttribute('data-id'));
+      currentList = todoLists.find((l) => l.id === targetId); // high order function
+      renderList();
+    });
+    document.querySelector('#lists-list').appendChild(li); // connect the element to the tree as a child of ul#lists-list
+  });
+
+  if (currentList === null) {
+    document.querySelector('#current-list-target').innerHTML =
+      'No list to display';
+  }
+  document.getElementById("add").onclick = function() {
+  //First things first, we need our text:
+  var text = document.getElementById("idea").value; //.value gets input values
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(text);
+  //Now construct a quick list element
+  node.appendChild(textnode);
+  //Now use appendChild and add it to the list!
+  document.getElementById("list").appendChild(node);
+  }

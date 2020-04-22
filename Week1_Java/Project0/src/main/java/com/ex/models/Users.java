fba460aab303a258915432fb92fb3a62e7da5e52
august@revature.com <Gregory Cox>
2020-04-22 09:15:51 -0400

    Project0 final

--- Week1_Java/Project0/src/main/java/com/ex/models/Users.java
+++
@@ -1,4 +0,0 @@
-package com.ex.models;
-
-public class Users {
-}
--- Week1_Java/Project0/src/main/java/com/ex/utils/ConnectionUtils.java
+++ Week1_Java/Project0/src/main/java/com/ex/utils/ConnectionUtils.java
@@ -1,4 +1,17 @@
 package com.ex.utils;
 
-public class ConnectionUtils {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class ConnectionUtils {
+    protected String url;
+    protected String username;
+    protected String password;
+    protected String defaultSchema ;
+
+    public abstract Connection getConnection() throws SQLException;
+
+    public String getDefaultSchema() {
+        return this.defaultSchema;
+    }
 }

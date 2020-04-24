086edf8b5db1f1eb6e9a6fbf5ca9c9896d0034a9
Joshua Brewer <jtb91594@gmail.com>
2020-04-24 16:59:30 -0400

    added student and teacher user models

---
+++ Project1/src/main/java/com/ex/models/Student_User.java
@@ -0,0 +1,12 @@
+package com.ex.models;
+
+public class Student_User extends User {
+	
+	public Student_User(String first_name, String last_name, String email, String password) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.password = password;
+	}
+
+}
---
+++ Project1/src/main/java/com/ex/models/Teacher_User.java
@@ -0,0 +1,12 @@
+package com.ex.models;
+
+public class Teacher_User extends User{
+	
+	public Teacher_User(String first_name, String last_name, String email, String password) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.password = password;
+	}
+
+}

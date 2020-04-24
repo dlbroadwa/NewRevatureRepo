e8973ab355f27b0842f86e17a6897d93497cf10f
AustinKind <austin.t.kind@gmail.com>
2020-04-24 17:12:44 -0500

    added submittedassignment model

---
+++ Project1/src/gradebook/models/SubmittedAssignment.java
@@ -0,0 +1,67 @@
+package gradebook.models;
+
+import java.time.LocalDateTime;
+
+public class SubmittedAssignment {
+	private int assignment_id;
+	private String course_id;
+	private String student_id;
+	private String file;
+	private LocalDateTime date_submitted;
+	
+	public SubmittedAssignment (int assignment_id, String course_id, String student_id, String file) {
+		this.assignment_id = assignment_id;
+		this.course_id = course_id;
+		this.student_id = student_id;
+		this.file = file;
+		this.date_submitted = LocalDateTime.now();
+	}
+	
+	public SubmittedAssignment (int assignment_id, String course_id, String student_id, String file, LocalDateTime date_submitted) {
+		this.assignment_id = assignment_id;
+		this.course_id = course_id;
+		this.student_id = student_id;
+		this.file = file;
+		this.date_submitted = date_submitted;
+	}
+
+	public int getAssignmentId() {
+		return assignment_id;
+	}
+
+	public void setAssignmentId(int assignment_id) {
+		this.assignment_id = assignment_id;
+	}
+
+	public String getCourseId() {
+		return course_id;
+	}
+
+	public void setCourseId(String course_id) {
+		this.course_id = course_id;
+	}
+
+	public String getStudentId() {
+		return student_id;
+	}
+
+	public void setStudentId(String student_id) {
+		this.student_id = student_id;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public LocalDateTime getDateSubmitted() {
+		return date_submitted;
+	}
+
+	public void setDateSubmitted(LocalDateTime date_submitted) {
+		this.date_submitted = date_submitted;
+	}
+}

3fd62156153310cbd62f1790c080be8bea3ac2be
Bart Martinon <barthelemy.martinon@gmail.com>
2020-04-24 18:01:43 -0400

    Began adding skeleton structure of Java application, models, app, user

---
+++ Project1/ShelterWebApp/src/main/java/app/Application.java
@@ -0,0 +1,20 @@
+package app;
+
+/**
+ *  Project 0:<br>
+ * <br>
+ *  The Application class serves as an abstract template for runnable applications.
+ *
+ *  <br> <br>
+ *  Created: <br>
+ *     24 April 2020, Barthelemy Martinon<br>
+ *     With assistance from: August Duet<br>
+ *  Modifications: <br>
+ *     24 April 2020, Barthelemy Martinon,    Created class.
+ * <br>
+ *  @author Barthelemy Martinon   With assistance from: August Duet
+ *  @version 24 April 2020
+ */
+public abstract class Application {
+    public abstract void run();
+}
---
+++ Project1/ShelterWebApp/src/main/java/user/User.java
@@ -0,0 +1,27 @@
+package user;
+
+/**
+ *  Project 1:<br>
+ * <br>
+ *  The User interfaces serves as an interface for all User types, authentification and activity when interacting with
+ *  	the shelter application.
+ *  As of 4/24/2020, it is expected that there will be two User types: Employee and Customer.
+ *  	Employees will have access to database manipulation, while Customers will only be able to view entries and
+ *  	submit requests.
+ *
+ *  <br> <br>
+ *  Created: <br>
+ *     24 April 2020, Barthelemy Martinon<br>
+ *     With assistance from: <br>
+ *  Modifications: <br>
+ *     09 April 2020, Barthelemy Martinon,    Created class.
+ *     										  Implemented userAuth method signature.
+ *                                            
+ * <br>
+ *  @author Barthelemy Martinon   With assistance from:
+ *  @version 24 April 2020
+ */
+
+public interface User {
+	public boolean userAuth(String username, String password);
+}

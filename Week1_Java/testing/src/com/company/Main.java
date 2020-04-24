51dafd141f29a0848f42577f9df1176379ba9a82
august@revature.com <Gregory Cox>
2020-04-24 15:49:10 -0400

    Get

---
+++ Week1_Java/testing/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        String x ="bob";
+//        String y=x;
+//        String b = new String("bob");
+//        String a = "bob";
+//        if (y==x)
+//            System.out.println("1");
+//        if (x==b)
+//            System.out.println("2");
+//        if (x==a)
+//            System.out.println("3");
+       char a = 'a';
+       a +=5;
+        System.out.println(a);
+    }
+}
---
+++ Week1_Java/testing/src/com/company/Question.java
@@ -0,0 +1,6 @@
+public class Question {
+    public static void myMethod(Object o) {}
+    public static void main(String[] args) {
+        myMethod(new String("Test"));
+    }
+}

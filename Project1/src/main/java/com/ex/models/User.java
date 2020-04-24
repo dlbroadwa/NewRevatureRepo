1449b7ac0d9d594e5800ef697ac3f29e34ae1ad7
Joshua Brewer <jtb91594@gmail.com>
2020-04-24 16:41:25 -0400

    added Project1 maven project

---
+++ Project1/src/main/java/com/ex/models/User.java
@@ -0,0 +1,37 @@
+package com.ex.models;
+
+public abstract class User {
+	protected String first_name;
+	protected String last_name;
+	protected String email;
+	protected String password;
+	
+	
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}

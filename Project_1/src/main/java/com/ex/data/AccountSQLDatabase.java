1a6aa51826c287644107aebb27533981d86732d6
Perry Lee <perrylee_2004@yahoo.com>
2020-04-28 18:00:09 -0400

    Implement CRUD for accounts database

--- Project_1/src/main/java/com/ex/data/AccountSQLDatabase.java
+++ Project_1/src/main/java/com/ex/data/AccountSQLDatabase.java
@@ -3,7 +3,12 @@ package com.ex.data;
 import com.ex.models.Account;
 import com.ex.utils.DatabaseConnection;
 
-public class AccountSQLDatabase implements GenericDAO<Account, Integer> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountSQLDatabase implements GenericDAO<Account, String> {
     private final DatabaseConnection dc;
 
     public AccountSQLDatabase(DatabaseConnection dc) {
@@ -12,21 +17,95 @@ public class AccountSQLDatabase implements GenericDAO<Account, Integer> {
 
     @Override
     public boolean add(Account newAccount) {
-        return false;
+        if (findByID(newAccount.getEmail()) != null)
+            return false;
+
+        int addedRowCount = 0;
+        String sql = "INSERT INTO " + dc.getSchema() +
+                ".accounts (name, email, password, is_employee, is_manager) values (?, ?, ?, ?, ?)";
+
+        try (Connection conn = dc.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, newAccount.getName());
+            ps.setString(2, newAccount.getEmail());
+            ps.setString(3, newAccount.getPassword());
+            ps.setBoolean(4, newAccount.getEmployee());
+            ps.setBoolean(5, newAccount.getManager());
+
+            addedRowCount = ps.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return addedRowCount == 1;
     }
 
     @Override
-    public Account findByID(Integer id) {
-        return null;
+    public Account findByID(String email) {
+        Account result = null;
+
+        String sql = "SELECT name, email, password, is_employee, is_manager FROM " +
+                dc.getSchema() + ".accounts WHERE email=?";
+
+        try (Connection conn = dc.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, email);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    result.setName(rs.getString("name"));
+                    result.setEmail(rs.getString("email"));
+                    result.setPassword(rs.getString("password"));
+                    result.setEmployee(rs.getBoolean("is_employee"));
+                    result.setManager(rs.getBoolean("is_manager"));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
-    public boolean update(Integer id, Account newAccount) {
-        return false;
+    public boolean update(String email, Account newAccount) {
+        int updatedRowCount = 0;
+
+        String sql = "UPDATE " + dc.getSchema() +
+                ".accounts SET name=?, email=?, password=?, is_employee=?, is_manager=? WHERE email=?";
+
+        try (Connection conn = dc.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, newAccount.getName());
+            ps.setString(2, newAccount.getEmail());
+            ps.setString(3, newAccount.getPassword());
+            ps.setBoolean(4, newAccount.getEmployee());
+            ps.setBoolean(5, newAccount.getManager());
+            ps.setString(6, email);
+
+            updatedRowCount = ps.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return updatedRowCount > 0;
     }
 
     @Override
-    public boolean remove(Integer id) {
-        return false;
+    public boolean remove(String email) {
+        int deletedRowCount = 0;
+
+        String sql = "DELETE FROM " + dc.getSchema() + ".accounts WHERE email=?";
+
+        try (Connection conn = dc.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, email);
+
+            deletedRowCount = ps.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return deletedRowCount > 0;
     }
 }

be92fcb2dff893dd070ac29a3b823170d0587802
AustinKind <austin.t.kind@gmail.com>
2020-04-28 16:18:08 -0500

    added connection util

---
+++ Project1/src/gradebook/util/ConnectionUtil.java
@@ -0,0 +1,22 @@
+package gradebook.util;
+
+import java.sql.Connection;
+
+/**
+ * ConnectionUtil --- Allows for a connection to be established.
+ * @author Austin Kind
+ */
+
+public abstract class ConnectionUtil {
+	
+	protected String url;
+	protected String username;
+	protected String password;
+	
+	/**
+	 * Establishes a connection.
+	 * @return 		Connection.
+	 */
+	public abstract Connection getConnection();
+
+}
---
+++ Project1/src/gradebook/util/PostgresConnectionUtil.java
@@ -0,0 +1,42 @@
+package gradebook.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * PostgresConnectionUtil --- Establishes a connection to the Postgres database.
+ * @author Austin Kind
+ */
+public class PostgresConnectionUtil extends ConnectionUtil {
+	
+	private static final String CONNECTION_USERNAME = System.getenv("CONNECTION_USERNAME");
+	private static final String CONNECTION_PASSWORD = System.getenv("CONNECTION_PASSWORD");
+	private static final String URL = System.getenv("CONNECTION_URL");
+	private static Connection connection;
+	
+	static {
+		try {
+			DriverManager.registerDriver(new org.postgresql.Driver());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Establishes a connection to the Postgres database.
+	 * @return 		Connection to the Postgres database.
+	 */
+	public synchronized Connection getConnection() {
+		try {
+			if (connection == null)
+				connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+			if (connection.isClosed()) {
+				connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}

7e3cf7c03485fe6b483047776c6308c7285714a4
jsev5000 <j.s.severance@gmail.com>
2020-04-29 09:49:27 -0600

    get items for customer and admin

---
+++ Proj 1/Bar_Inv/src/main/java/com/company/DAO/data/ItemsDAO.java
@@ -0,0 +1,16 @@
+package com.company.DAO.data;
+
+import com.company.DAO.DatabaseConnection;
+import com.company.DAO.models.Item;
+import com.company.DAO.utils.ConnectionUtils;
+import com.company.DAO.utils.PostgresqlConnectionUtils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ItemsDAO {
+    private ItemsDAO(){}
+
+}
+
+
---
+++ Proj 1/Bar_Inv/src/main/java/com/company/DAO/servlets/GetAllItemsAdmin.java
@@ -0,0 +1,48 @@
+package com.company.DAO.servlets;
+
+import com.company.DAO.data.ItemRepository;
+import com.company.DAO.data.ItemsDAO;
+import com.company.DAO.data.Repository;
+import com.company.DAO.models.Item;
+import com.company.app.BarInventoryApplication;
+import org.apache.logging.log4j.core.appender.rewrite.MapRewritePolicy;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+public class GetAllItemsAdmin extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        List<Item> tmp = null; //hmmm... gotta figure this out
+//        try {
+//            tmp = ItemsDAO.findAll();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        //create a table and fill it
+        //create table and first row with the header
+        out.print("<h2>View Inventory</h2>");
+        out.print("<table border='1' width='100%'");
+        out.print("<tr><th>Item Name</th><th>Item ID</th><th>Number in Stock</th>"+
+                "<th>Low Level</th><th>Optimal Level</th></tr>");
+        //fill the table
+        for (Item i:tmp){
+            out.print("<tr><td>"+i.getItemName()+"</td><td>"+i.getId()+"</td><td>"+i.getOnHand()+"</td>"+
+                    "<td>"+i.getLowLevel()+"</td><td>"+i.getOptLevel()+"</td></tr>");
+        }
+        out.print("</table>");
+
+        out.close();
+    }
+}

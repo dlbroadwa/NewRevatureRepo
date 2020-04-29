d436040f7242062417cb9bd348a3b3db5fd7fc0c
AustinKind <austin.t.kind@gmail.com>
2020-04-29 14:38:07 -0500

    Finished Assignments DAOs

--- Project1/src/gradebook/dao/AssignmentsDAO.java
+++ Project1/src/gradebook/dao/AssignmentsDAO.java
@@ -1,10 +1,47 @@
 package gradebook.dao;
 
+import java.util.List;
+
 import gradebook.models.Assignment;
 
+/**
+ * AssignmentstDAO --- Accesses data from an assignments table.
+ * @author Austin Kind
+ */
 public interface AssignmentsDAO {
-	Assignment getAssignmentsByCourse(String course_id);
-	void addAssignment(Assignment assignment);
-	void updateAssignment(Assignment assignment);
-	void deleteAssignment(Assignment assignment);
+	
+	/**
+	 * Retrieves all the assignments from a course by the course's ID.
+	 * @param course_id		The course ID.
+	 * @return				A list of Assignments from the given course.
+	 */
+	List<Assignment> getAssignmentsByCourse(String course_id);
+	
+	/**
+	 * Retrieves an assignment given the assignment ID.
+	 * @param assignment_id		The ID of the desired assignment.
+	 * @return					The assignment requested.
+	 */
+	Assignment getAssignmentById(int assignment_id);
+	
+	/**
+	 * Adds the given assignment to the table.
+	 * @param assignment	The assignment to be added.
+	 * @return				True if operation was successful.
+	 */
+	boolean addAssignment(Assignment assignment);
+	
+	/**
+	 * Updates the given assignment to the table.
+	 * @param assignment	The assignment to be updated.
+	 * @return				True if operation was successful.
+	 */
+	boolean updateAssignment(Assignment assignment);
+	
+	/**
+	 * Deletes the given assignment from the table.
+	 * @param assignment	The assignment to be deleted.
+	 * @return				True if operation was successful.
+	 */
+	boolean deleteAssignment(Assignment assignment);
 }

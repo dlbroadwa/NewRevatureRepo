978ff8b4ff54740cb5c6cb4df14f448dfa80b473
AustinKind <austin.t.kind@gmail.com>
2020-04-29 14:37:44 -0500

    Finished courses DAOs

--- Project1/src/gradebook/dao/CoursesDAO.java
+++ Project1/src/gradebook/dao/CoursesDAO.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 import gradebook.models.Course;
 
+/**
+ * CoursesDAO --- Accesses data from a courses table.
+ * @author Austin
+ */
 public interface CoursesDAO {
+	
+	/**
+	 * Retrieves a course matching the given course ID.
+	 * @param course_id		The ID of the desired course.
+	 * @return				The course desired.
+	 */
 	Course getCourseById(String course_id);
+	
+	/**
+	 * Retrieves all courses taught be the given teacher.
+	 * @param teacher_id	The ID of the given teacher.
+	 * @return				A list of courses taught by the given teacher.
+	 */
 	List<Course> getCoursesByTeacher(String teacher_id);
 }

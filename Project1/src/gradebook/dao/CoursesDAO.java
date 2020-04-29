fc6c58a2cb3a391daaae34911c5216ffecd706e2
AustinKind <austin.t.kind@gmail.com>
2020-04-29 13:37:06 -0500

    finished CourseDAO

--- Project1/src/gradebook/dao/CoursesDAO.java
+++ Project1/src/gradebook/dao/CoursesDAO.java
@@ -1,8 +1,10 @@
 package gradebook.dao;
 
+import java.util.List;
+
 import gradebook.models.Course;
 
 public interface CoursesDAO {
 	Course getCourseById(String course_id);
-	Course getCoursesByTeacher(String teacher_id);
+	List<Course> getCoursesByTeacher(String teacher_id);
 }
--- Project1/src/gradebook/dao/CoursesSQLDAO.java
+++ Project1/src/gradebook/dao/CoursesSQLDAO.java
@@ -1,19 +1,83 @@
 package gradebook.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import gradebook.models.Course;
+import gradebook.util.ConnectionProvider;
 
 public class CoursesSQLDAO implements CoursesDAO {
 
+	private Connection conn;
+	PreparedStatement statement;
+	
 	@Override
 	public Course getCourseById(String course_id) {
-		// TODO Auto-generated method stub
-		return null;
+		Course course = null;
+		
+		try {
+			Connection conn = ConnectionProvider.getConnection();
+			String sql = "SELECT * FROM public.courses WHERE course_id=?;";
+			statement = conn.prepareStatement(sql);
+			statement.setString(1, course_id);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				String courseId = rs.getString("course_id");
+				String name = rs.getString("name");
+				String teacher_id = rs.getString("teacher_id");
+				course = new Course(courseId, name, teacher_id);
+			}	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResources();
+		}
+		
+		return course;
 	}
 
 	@Override
-	public Course getCoursesByTeacher(String teacher_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Course> getCoursesByTeacher(String teacher_id) {
+		List<Course> courses = new ArrayList<>();
+		
+		try {
+			Connection conn = ConnectionProvider.getConnection();
+			String sql = "SELECT * FROM public.courses WHERE teacher_id=?;";
+			statement = conn.prepareStatement(sql);
+			statement.setString(1, teacher_id);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				String courseId = rs.getString("course_id");
+				String name = rs.getString("name");
+				String teacherId = rs.getString("teacher_id");
+				courses.add(new Course(courseId, name, teacherId));
+			}	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResources();
+		}
+		
+		return courses;
 	}
 
+	private void closeResources() {
+		try {
+			if (statement != null)
+				statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }

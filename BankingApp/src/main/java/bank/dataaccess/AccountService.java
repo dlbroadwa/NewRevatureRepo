b5ecbb9155372eb64171d7566fc53ef1f8499023
dylanchhinn <dylanchhinn@gmail.com>
2020-04-29 13:17:42 -0700

    Added test Servlet

---
+++ BankingApp/src/main/java/bank/dataaccess/AccountService.java
@@ -0,0 +1,18 @@
+package bank.dataaccess;
+
+import java.time.LocalDate;
+
+public class AccountService {
+
+    public void deposit(int id, double amount, double accountId) {
+
+    }
+
+    public void withdraw(int id, double amount) {
+        double currentAmount = 0 - amount;
+    }
+
+    public void transfer(int id, double amount) {
+
+    }
+}

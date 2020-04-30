25d425d7fa22eb0549f29b25c2b5199a437b3303
dyltra <35568707+dyltra@users.noreply.github.com>
2020-04-30 17:44:23 -0400

    deleted authentication status

--- project1/src/main/java/com/game/service/accountservices/UserAuthentication.java
+++
@@ -1,8 +0,0 @@
-package com.game.service.accountservices;
-
-import com.game.system.AuthenticationStatus;
-
-public interface UserAuthentication {
-    void authenticate(String username, String password,
-                      AuthenticationStatus onStatusChange);
-}

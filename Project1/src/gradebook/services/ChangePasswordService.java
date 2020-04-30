f3f219a22efa90e14eec41b16a879398d1444b00
AustinKind <austin.t.kind@gmail.com>
2020-04-30 16:25:26 -0500

    implemented service

--- Project1/src/gradebook/services/ChangePasswordService.java
+++ Project1/src/gradebook/services/ChangePasswordService.java
@@ -1,8 +1,34 @@
 package gradebook.services;
 
+import gradebook.dao.StudentSQLDAO;
+import gradebook.dao.TeacherSQLDAO;
+import gradebook.dao.UserDAO;
+import gradebook.models.User;
+import gradebook.models.Teacher_User;
+import gradebook.models.Student_User;
+import gradebook.util.Regex;
+
 public class ChangePasswordService {
 	
+	private UserDAO<User,String> teacherDAO;
+	private UserDAO<User,String> studentDAO;
+	
 	public ChangePasswordService() {
-		//TODO
+		this.teacherDAO = TeacherSQLDAO.getInstance();
+		this.studentDAO = StudentSQLDAO.getInstance();
+	}
+	
+	public boolean changePassword(User user, String newPassword) {
+		if (Regex.isValidPassword(newPassword)) {
+			user.setPassword(newPassword);
+			if (user instanceof Teacher_User) {
+				teacherDAO.updateUser(user);
+				return true;
+			} else if (user instanceof Student_User) {
+				studentDAO.updateUser(user);
+				return true;
+			}
+		}
+		return false;
 	}
 }

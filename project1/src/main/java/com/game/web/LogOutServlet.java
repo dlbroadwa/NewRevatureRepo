23f57b3d4d32650f1e91128834434827cc14cc54
rjefferson <reginald.jefferson@outlook.com>
2020-05-04 16:39:38 -0400

    added logout servlet

---
+++ project1/src/main/java/com/game/web/LogOutServlet.java
@@ -0,0 +1,22 @@
+package com.game.web;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LogOutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req. getSession(false);
+        if (session != null) {
+            session.removeAttribute("username");
+            RequestDispatcher rs = req.getRequestDispatcher("signup.html");
+            rs.forward(req, resp);
+        }
+
+    }
+}

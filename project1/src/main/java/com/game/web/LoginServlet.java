3815053b59ce0e02f741353a2116474c1c81e0e8
rjefferson <reginald.jefferson@outlook.com>
2020-05-04 16:34:09 -0400

    cleaneed up login servlet

--- project1/src/main/java/com/game/web/LoginServlet.java
+++ project1/src/main/java/com/game/web/LoginServlet.java
@@ -1,31 +1,36 @@
 package com.game.web;
 
 import com.game.service.accountservices.AccountDetailService;
-
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import com.game.system.PortalContextListener;
 
 public class LoginServlet extends HttpServlet {
     AccountDetailService accountDetailService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        ServletContext context = config.getServletContext();
+        accountDetailService = (AccountDetailService) context.getAttribute("accountDetailService");
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String destPage = "signup.html";
-//        Possible login/logout servlet to be added
-//        LoginDao userDao = new UserDao();
+
         if(accountDetailService.checkCredentials(username, password)) {
             HttpSession session = req.getSession();
             session.setAttribute("username", username);
             destPage = "portal.html";
-            RequestDispatcher rs = req.getRequestDispatcher("Welocme");
+            RequestDispatcher rs = req.getRequestDispatcher("Welcome" + username);
             rs.forward(req, resp);
         } else {
             resp.getWriter().write("Username or Password is incorrect. Please try again");

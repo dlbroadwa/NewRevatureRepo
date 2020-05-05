3e69064d3ce2e96832432a4904ebfe0809997a6d
Shawyn <shawynkane@users.noreply.github.com>
2020-05-05 15:02:28 -0500

    I worked on setting up cookies for keeping track of the logged in user.

--- BankingApp/src/main/java/bank/servlets/LoginServlet.java
+++ BankingApp/src/main/java/bank/servlets/LoginServlet.java
@@ -1,22 +1,21 @@
 package bank.servlets;
 
-
-import bank.model.User;
 import bank.services.UserServices;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /***
  *
  * @author Shawyn Kane
  */
 public class LoginServlet extends HttpServlet {
-    UserServices us;
+    private UserServices us;
+    private final int COOKIE_MAX_AGE = 300;
 
     @Override
     public void init() throws ServletException {
@@ -29,7 +28,18 @@ public class LoginServlet extends HttpServlet {
         System.out.println(req.getContentType());
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        if (us.login(email, password)) resp.setStatus(201);
+        if (us.login(email, password)) {
+            Cookie authenticatedUserEmailCookie = new Cookie("userEmail", email);
+            authenticatedUserEmailCookie.setMaxAge(COOKIE_MAX_AGE);
+            resp.addCookie(authenticatedUserEmailCookie);
+            resp.setStatus(201);
+        }
         else resp.setStatus(206);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+
     }
 }

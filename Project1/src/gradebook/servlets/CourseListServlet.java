c63b1b89b2d1b54497bd0cc1e826b2cd56bb2236
AustinKind <austin.t.kind@gmail.com>
2020-05-06 01:24:30 -0500

    some progress

--- Project1/src/gradebook/servlets/CourseListServlet.java
+++ Project1/src/gradebook/servlets/CourseListServlet.java
@@ -1,17 +1,32 @@
 package gradebook.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import gradebook.models.Course;
+import gradebook.services.CourseService;
 
 public class CourseListServlet extends HttpServlet {
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		HttpSession session = req.getSession();
+		String username = (String)(session.getAttribute("user_id"));
+		// if invalid session, redirect to login
+		if (username == null) {
+			resp.sendRedirect("index.html");
+		}
+		CourseService cs = new CourseService();
+		List<Course> courses = cs.getCourses(username);
+		out.println("<h2>Welcome " + username + "!</h2>");
 	}
 }

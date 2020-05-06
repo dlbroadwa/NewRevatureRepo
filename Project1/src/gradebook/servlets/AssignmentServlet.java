f029f0b73f78294f1733e102316b63fc5326f348
Joshua Brewer <jtb91594@gmail.com>
2020-05-06 16:47:28 -0400

    added AJAX functionality to grade-submission.html, mappings to the web.xml and started the Assignment servlet

--- Project1/src/gradebook/servlets/AssignmentServlet.java
+++ Project1/src/gradebook/servlets/AssignmentServlet.java
@@ -1,19 +1,40 @@
 package gradebook.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import gradebook.services.AssignmentService;
 
 /**
  * Servlet implementation class AssignmentServlet
  */
 public class AssignmentServlet extends HttpServlet {
-
+	AssignmentService as = new AssignmentService();
+	
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		Integer assignmentId = (Integer)session.getAttribute("assignment_id");
+		if(assignmentId == null) {
+			resp.getWriter().write("400");
+		}
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		String userId = (String)session.getAttribute("user_id");
+		String courseId = (String)session.getAttribute("course_id");
+		int assignmentId = Integer.parseInt(req.getParameter("assignment_id"));
+		
+		
+		
+		session.setAttribute("assignment_id", assignmentId);
 	}
 }

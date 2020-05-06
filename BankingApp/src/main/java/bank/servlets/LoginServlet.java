6d9cf1a64b91fa827bcd64064e3be1b2c5a1c56c
Shawyn <shawynkane@users.noreply.github.com>
2020-05-06 13:59:42 -0500

    replaced cookie with http session

--- BankingApp/src/main/java/bank/servlets/LoginServlet.java
+++ BankingApp/src/main/java/bank/servlets/LoginServlet.java
@@ -3,10 +3,7 @@ package bank.servlets;
 import bank.services.UserServices;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 /***
@@ -15,7 +12,7 @@ import java.io.IOException;
  */
 public class LoginServlet extends HttpServlet {
     private UserServices us;
-    private final int COOKIE_MAX_AGE = 300;
+    private final int SESSION_MAX_AGE = 300;
 
     @Override
     public void init() throws ServletException {
@@ -29,9 +26,9 @@ public class LoginServlet extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         if (us.login(email, password)) {
-            Cookie authenticatedUserEmailCookie = new Cookie("userEmail", email);
-            authenticatedUserEmailCookie.setMaxAge(COOKIE_MAX_AGE);
-            resp.addCookie(authenticatedUserEmailCookie);
+            HttpSession session = req.getSession();
+            session.setMaxInactiveInterval(SESSION_MAX_AGE);
+            session.setAttribute("userEmail", email);
             resp.setStatus(201);
         }
         else resp.setStatus(206);

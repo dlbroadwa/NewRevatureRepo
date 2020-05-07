8bc263fdc625eec6ff5841cb2f1f2efbbd828489
Bart Martinon <barthelemy.martinon@gmail.com>
2020-05-06 20:35:18 -0400

    Added getter to petList

--- Project1/ShelterWebApp/Project1/src/main/java/models/pet/PetList.java
+++ Project1/ShelterWebApp/Project1/src/main/java/models/pet/PetList.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PetList {
+    // Instance Variables
     public List<Pet> pets;
 
+    // Constructors
     public PetList() { pets = new ArrayList<>(); }
 
     public PetList(ArrayList<Pet> petlist) { pets = petlist; }
+
+    // Getter Methods
+
+    public List<Pet> getPets() { return pets; }
 
     @Override
     public String toString() {

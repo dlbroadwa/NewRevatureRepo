77d3dc8316e851d0a4ea331d32ae35d1cb0fddd1
Christopher Hill <chrimhill@gmail.com>
2020-05-07 04:45:37 -0500

    manager's homepage has made some progress

--- ERS_Project1/src/main/java/com/ex/ers/newServlets/AllReimsForID.java
+++ ERS_Project1/src/main/java/com/ex/ers/newServlets/AllReimsForID.java
@@ -14,22 +14,17 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/AllReimsForIDManager")
-public class AllReimsForIDManager extends HttpServlet {
+@WebServlet("/AllReimsForIDNEW")
+public class AllReimsForID extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ReimbursementService service = new ReimbursementService();
-        HttpSession session = req.getSession();
         ServletOutputStream out = resp.getOutputStream();
         resp.setContentType("application/json;charset=UTF-8");
-
         int id = Integer.parseInt(req.getParameter("selectedID")); //they will choose an employee, and the request will contain the id for said emp
-        List<ReimbursementRequest> all = null;
-        all = service.getAllForId(id);
-
+        List<ReimbursementRequest> all = service.getAllForId(id);
         String output = new Gson().toJson(all);
         out.print(output);
-        //resp.sendRedirect("manager_employeeView.html");
+        }
     }
-}
 

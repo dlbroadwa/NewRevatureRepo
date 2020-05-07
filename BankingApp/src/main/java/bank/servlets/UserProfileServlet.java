6503186b8fc3735a9b3e2872e417835bffcc009d
dylanchhinn <dylanchhinn@gmail.com>
2020-05-07 12:51:52 -0700

    Added some profile page

--- BankingApp/src/main/java/bank/servlets/UserProfileServlet.java
+++ BankingApp/src/main/java/bank/servlets/UserProfileServlet.java
@@ -4,10 +4,12 @@ import bank.model.User;
 import bank.services.UserServices;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.mortbay.util.ajax.JSON;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class UserProfileServlet extends HttpServlet {
     UserServices us;
@@ -19,11 +21,15 @@ public class UserProfileServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JSONObject job = new JSONObject();
         JSONArray jary = new JSONArray();
+        ArrayList<User> users = new ArrayList<User>();
         String email = req.getSession().getAttribute("userEmail").toString();
         User user = us.retrieveUserByEmail(email);
-        user.setPassword("***");
-        jary.put(user);
+        job.put("firstname", user.getEmail());
+        job.put("lastname", user.getLastName());
+        job.put("phone", user.getPhoneNumber());
+        jary.put(job);
         resp.getWriter().write(jary.toString());
     }
 

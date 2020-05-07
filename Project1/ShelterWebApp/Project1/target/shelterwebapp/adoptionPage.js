6216674e82314f76e3a596b2ed08d13a5e5caa53
Bart Martinon <barthelemy.martinon@gmail.com>
2020-05-07 14:55:56 -0400

    Table printing successfully, now for the conditional statements

--- Project1/ShelterWebApp/Project1/target/shelterwebapp/adoptionPage.js
+++ Project1/ShelterWebApp/Project1/target/shelterwebapp/adoptionPage.js
@@ -8,10 +8,45 @@ $(function() {
 
                 $.each(data, function(key, val) {
 
+                    var selectedGender = document.getElementById("gender").getSelection("gender");
 
-                        items.push('<li id="' + key + '">' + val.name + '</li>');
+                    var selectedAge = document.getElementById("age").getSelection("age");
 
-                  
+                    var selectedType = document.getElementById("type").getSelection("type");
+
+                    if(selectedType === "any" && selectedGender === "any" && selectedAge === 6){
+
+                        items.push('<li id="' + key + '">' +val.id+ val.type + val.name + val.age +val.breed + val.gender + '</li>');
+
+                    }
+
+
+                    if(selectedType === "any"){
+
+                        if (selectedGender === val.gender || selectedGender === "any") {
+
+                            if (selectedAge === val.age || selectedAge ==="any") {
+
+                                items.push('<li id="' + key + '">' + val.id + val.type + val.name + val.age + val.breed + val.gender + '</li>');
+
+                            }
+
+                        }
+
+                    }
+
+                    if(selectedType === val.type) {
+
+                        if (selectedGender === val.gender || selectedGender === "any") {
+
+                            if (selectedAge === val.age || selectedAge ==="any") {
+
+                                items.push('<li id="' + key + '">' + val.id + val.type + val.name + val.age + val.breed + val.gender + '</li>');
+
+                            }
+
+                        }
+                    }
 
                 });
 

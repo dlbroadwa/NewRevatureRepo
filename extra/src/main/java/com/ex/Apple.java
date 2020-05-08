3d205a82ab58a8863b8560f3cdf4ab4e3bf04d41
Daniel Wallace <walldiv@gmail.com>
2020-05-08 18:16:48 -0400

    Initial Project Structure - Project2 for ThatTeam

--- extra/src/main/java/com/ex/Apple.java
+++
@@ -1,8 +0,0 @@
-package com.ex;
-
-public class Apple extends Fruit{
-  public Apple() {
-    this.type = "apple";
-    this.color = "red";
-  }
-}
--- extra/src/main/java/com/ex/Orange.java
+++
@@ -1,8 +0,0 @@
-package com.ex;
-
-public class Orange extends Fruit {
-  public Orange() {
-    this.color = "orange";
-    this.type = "orange";
-  }
-}

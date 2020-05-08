e5cd90968446d7441618fe9c2c7053f5bf0a68bf
dyltra <35568707+dyltra@users.noreply.github.com>
2020-05-07 21:05:25 -0400

    use send redirects instead

--- project1/src/main/java/com/game/web/MessageServlet.java
+++ project1/src/main/java/com/game/web/MessageServlet.java
@@ -1,10 +1,13 @@
 package com.game.web;
 
 import com.game.models.Message;
+import com.game.service.accountservices.AccountDetailService;
+import com.game.service.accountservices.AccountDetailServiceImp;
 import com.game.service.accountservices.ModificationService;
 import com.game.service.messageservices.MessageService;
 import com.google.gson.Gson;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -37,13 +40,12 @@ public class MessageServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-
         String username = (String) session.getAttribute("username");
-        String toUser = req.getParameter("toUser");
-        String content = req.getParameter("content");
-
-        resp.getWriter().write("Message sent.");
-        messageService.send(toUser, content, username);
+        String toUser = req.getParameter("toWho");
+        String content = req.getParameter("message");
+        messageService.send(username, toUser, content);
+        //if we want website-wide messaging, include url from the request
+        resp.sendRedirect("pages/portal.html");
     }
 
     /**

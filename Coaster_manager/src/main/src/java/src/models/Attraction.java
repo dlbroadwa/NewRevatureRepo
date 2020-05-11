4fa002c6daa306522360d297288af3466d6aff5c
Paityn Maynard <paityn.maynard@gmail.com>
2020-05-11 13:09:55 -0400

    Adding Manager Service Models

---
+++ Coaster_manager/src/main/src/java/src/models/Attraction.java
@@ -0,0 +1,62 @@
+package models;
+
+/**
+ * Attraction Class
+ * Created by:Paityn Maynard on May 11,2020
+ */
+public class Attraction {//Start of Attractions class
+//Instance Variables
+    String name, status, imageurl;
+    int id, rating;
+//Constructors
+    public Attraction(){}
+
+    public Attraction(String name, String status, String imageurl, int id, int rating){
+        this.name=name;
+        this.status=status;
+        this.imageurl = imageurl;
+        this.id=id;
+        this.rating=rating;
+    }
+//Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+//Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}//End of Attractions Class

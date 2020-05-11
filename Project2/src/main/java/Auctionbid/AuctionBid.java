1043210256429e901b625236af854f36b391e008
dlbroadwa <>
2020-05-11 17:47:51 -0500

    Auction Class

---
+++ Project2/src/main/java/Auctionbid/AuctionBid.java
@@ -0,0 +1,25 @@
+package Auctionbid;
+
+public class AuctionBid {
+    private int auctionId;
+    private int bidderID;
+
+    public int getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(int auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public int getBidderID() {
+        return bidderID;
+    }
+
+    public void setBidderID(int bidderID) {
+        this.bidderID = bidderID;
+    }
+
+
+
+}

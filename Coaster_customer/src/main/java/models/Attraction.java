ff4102044b3375a49d1320493f33f614525ed551
Ray <vakilrtech@gmail.com>
2020-05-11 14:34:56 -0400

    added customer models

---
+++ Coaster_customer/src/main/java/models/Attraction.java
@@ -0,0 +1,49 @@
+package main.java.models;
+
+/**
+ *  Project 2:<br>
+ * <br>
+ *  The Ticket class serves as a representation of a real-world ticket used for interacting with the system.
+ *  	Ticket instances hold information of its real-world counterpart as variables.
+ *
+ *  <br> <br>
+ *  Created: <br>
+ *     11 May 2020, Barthelemy Martinon<br>
+ *     With assistance from: <br>
+ *  Modifications: <br>
+ *     11 May 2020, Barthelemy Martinon,    Created class.
+ * <br>
+ *  @author Barthelemy Martinon   With assistance from:
+ *  @version 11 May 2020
+ */
+public class Attraction {
+    // Instance Variables
+    private String name;
+    private String status;      // Different to Paityn's Attraction status, ex. "Open", "Closed", "Out of Order"
+    private String imageurl;
+    private int id;
+    private int rating;
+
+    // Constructor
+    public Attraction (String name, String status, String imageurl, int id, int rating){
+        this.name=name;
+        this.status=status;
+        this.imageurl = imageurl;
+        this.id=id;
+        this.rating=rating;
+    }
+
+    // Getter Methods
+    public String getName() { return name; }
+    public String getStatus() { return status; }
+    public String getImageurl() { return imageurl; }
+    public int getId() { return id; }
+    public int getRating() { return rating; }
+
+    // Setter Methods
+    public void setName(String name) { this.name = name; }
+    public void setStatus(String status) { this.status = status; }
+    public void setImageurl(String imageurl) { this.imageurl = imageurl; }
+    public void setId(int id) { this.id = id; }
+    public void setRating(int rating) { this.rating = rating; }
+}

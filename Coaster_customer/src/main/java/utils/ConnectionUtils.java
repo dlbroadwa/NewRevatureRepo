6311522a94db77a49f236c1cf18ac4381b447e88
Joshua Brewer <jtb91594@gmail.com>
2020-05-11 14:57:04 -0400

    added comments to connection utils

--- Coaster_customer/src/main/java/utils/ConnectionUtils.java
+++ Coaster_customer/src/main/java/utils/ConnectionUtils.java
@@ -3,6 +3,12 @@ package main.java.utils;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * ConnectionUtils - This is an abstract class that allows connections to different types of SQL
+ * database implementations.
+ * 
+ * @author Joshua Brewer
+ */
 public abstract class ConnectionUtils {
   protected String url;
   protected String username;

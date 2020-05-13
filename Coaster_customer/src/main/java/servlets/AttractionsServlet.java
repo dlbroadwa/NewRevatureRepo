bf8ec56c74fabe9338f35910ec7059ab7016b03a
Jean Aldoph II <danielaldoph@yahoo.com>
2020-05-13 16:10:40 -0400

    Added Attraction Servlet:Customer's JDoc

--- Coaster_customer/src/main/java/servlets/AttractionsServlet.java
+++ Coaster_customer/src/main/java/servlets/AttractionsServlet.java
@@ -14,8 +14,31 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Initialized by Jean Aldoph II
+ * The Servlet for all API calls involving Attractions that can be seen by the Customer.
+ * This Servlet only implements one Method, doGet
+ * This is the case because our customers should have no updating or posting privileges to our DB
+ *
+ *version 0.0: Jean D Aldoph II
+ */
+
+
+
 public class AttractionsServlet extends HttpServlet
 {
+    /**
+     * This Method is the only method implemented by Customer for attractions.
+     * This method takes an httpRequest and calls to the DAO for our Attractions.
+     * The customer Attraction DAO can only be used to view our Current Attractions, to alleviate confusion,
+     * Or allow customers to view a Specific attraction.
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         {
@@ -32,8 +55,10 @@ public class AttractionsServlet extends HttpServlet
                 resp.setCharacterEncoding("UTF-8");
                 resp.getWriter().write(json);
             }
-            else if (!(req.getHeader("find") == null)) {
-                try {
+            else if (!(req.getHeader("find") == null))
+            {
+                try
+                {
                     String json = null;
                     Attraction temp = repo.findById(new Integer(req.getHeader("find")));
                     Map<String, Attraction> options = new LinkedHashMap<>();
@@ -43,41 +68,84 @@ public class AttractionsServlet extends HttpServlet
                     resp.setContentType("application/json");
                     resp.setCharacterEncoding("UTF-8");
                     resp.getWriter().write(json);
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
+                }
+                catch (NumberFormatException e)
+                {
                     e.printStackTrace();
                 }
-
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
+    /**
+     * Not implemented
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doHead(req, resp);
     }
 
+    /**
+     * Not Implemented
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
     }
 
+    /**
+     * Not Implemented
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPut(req, resp);
     }
 
+    /**
+     * Not implemented
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doDelete(req, resp);
     }
-
+    /**
+     * Not implemented
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     public void destroy() {
         super.destroy();
     }
-
+    /**
+     * Not implemented
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     public void init() throws ServletException {
         super.init();

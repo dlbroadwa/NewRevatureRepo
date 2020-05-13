f378937e3653a51aeb0ed252db5db8a98e79a918
Paityn Maynard <paityn.maynard@gmail.com>
2020-05-13 12:11:29 -0400

    update

---
+++ Coaster_manager/src/main/java/servlets/AttractionsServlet.java
@@ -0,0 +1,53 @@
+package servlets;
+
+import models.Attraction;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AttractionsServlet extends HttpServlet {
+//Instance Variables
+    Attraction attraction = new Attraction();
+//Methods
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonObject data = new Gson().fromJson(req.getReader(), JsonObject.class);
+        String json = null;
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+      
+
+
+        resp.getWriter().write(json);
+    }
+
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}

ccd08cebe0864e0d880a7c2ffa9dbddf9e860583
Jordan <j.s.severance@gmail.com>
2020-05-15 16:13:52 -0600

    placeItem and removeItem servlets are rockin

--- ProjectTwo/GeoCache/src/main/java/com/ex/application/Main.java
+++ ProjectTwo/GeoCache/src/main/java/com/ex/application/Main.java
@@ -15,6 +15,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        GpsService service = new GpsService();
+        GeoCashe cashe = service.findCasheByID(16);
+        Item item = new Item("itemName","description","image");
 
+        GeoCasheHistorys history = new GeoCasheHistorys("person1@email.com",cashe,LocalDateTime.now(),"put it!",3);
+
+        service.placeItem(item,history);
     }
 }

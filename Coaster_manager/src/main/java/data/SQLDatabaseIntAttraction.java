4dad0dffc637d87db37cf0436c7e4504b8487f60
Paityn Maynard <paityn.maynard@gmail.com>
2020-05-19 16:39:27 -0400

    tests

--- Coaster_manager/src/main/java/data/SQLDatabaseIntAttraction.java
+++ Coaster_manager/src/main/java/data/SQLDatabaseIntAttraction.java
@@ -46,9 +46,10 @@ public class SQLDatabaseIntAttraction implements GenericDAO<Attraction,Integer> 
         List<Attraction> results = null;
         String schema = connectionUtil.getDefaultSchema();
 
-        String sql = "Select name,attractions.attractionid,imageurl,ratings,status, isactive from "+
-                      schema+".attractions left join " + schema +
-                     ".maintenance_tickets on attractions.attractionid = maintenance_tickets.attractionid";
+        String sql = "select name,att.attractionid,imageurl,ratings, status from "+schema+".attractions as att"
+                    + "left outer join"+ schema +".maintenance_tickets as mt "
+                    + "on att.attractionid = mt.attractionid"
+                    +"where mt.isactive or mt.isactive is null";
 
         try (Connection conn = connectionUtil.getConnection();
              Statement st = conn.createStatement();
@@ -56,7 +57,6 @@ public class SQLDatabaseIntAttraction implements GenericDAO<Attraction,Integer> 
             results = new ArrayList<>();
 
             while (rs.next()) {
-                if(rs.getBoolean("isactive")||rs.getString("status")==null) {
                     String name = rs.getString("name");
                     int id = rs.getInt("attractionid");
                     String status = rs.getString("status");
@@ -67,7 +67,6 @@ public class SQLDatabaseIntAttraction implements GenericDAO<Attraction,Integer> 
                     int rating = rs.getInt("ratings");
 
                     results.add(new Attraction(name, status, imageurl, id, rating));
-                }
             }
         }//End of try
         catch (SQLException throwables) {//Start of catch
@@ -118,9 +117,10 @@ public class SQLDatabaseIntAttraction implements GenericDAO<Attraction,Integer> 
         String schema = connectionUtil.getDefaultSchema();
         Attraction result = null;
 
-        String sql ="Select name,attractions.attractionid,imageurl,ratings,status from "+ schema +
-                    ".attractions left join "+schema+
-                    ".maintenance_tickets on attractions.attractionid = maintenance_tickets.attractionid where attractions.attractionid= ? ";
+        String sql = "select name,att.attractionid,imageurl,ratings, status from "+schema+".attractions as att"
+                + "left outer join"+ schema +".maintenance_tickets as mt "
+                + "on att.attractionid = mt.attractionid"
+                +"where mt.isactive or mt.isactive is null and att.attractionid=?";
 
         try (Connection conn = connectionUtil.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {//Start of first try
@@ -128,14 +128,12 @@ public class SQLDatabaseIntAttraction implements GenericDAO<Attraction,Integer> 
 
             try (ResultSet rs = ps.executeQuery()) {//Start of second try
                 if (rs.next()) {//Start of first if
-                    if(rs.getBoolean("isactive")||rs.getString("status")==null) {
                         result = new Attraction();
                         result.setName(rs.getString("name"));
                         result.setId(rs.getInt("attractionid"));
                         result.setImageurl(rs.getString("imageurl"));
                         result.setRating(rs.getInt("ratings"));
                         result.setStatus(rs.getString("status"));
-                    }
                 }//End of first if
             }//End of second try
         }//End of first try

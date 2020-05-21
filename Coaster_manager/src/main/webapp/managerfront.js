7f1f467b1bcc0f8d3107eadb54521e97ff2d3854
Paityn Maynard <paityn.maynard@gmail.com>
2020-05-21 16:38:27 -0400

    tests

--- Coaster_manager/src/main/webapp/managerfront.js
+++ Coaster_manager/src/main/webapp/managerfront.js
@@ -1,6 +1,6 @@
 let attractionCache;
 
-function changeHTML(){
+function getAttractions(){
 const httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
@@ -9,27 +9,27 @@ const httpRequest = new XMLHttpRequest();
         return null;
     }
  httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                attractionCache = this.response;
-                localStorage.setItem('attractionCache', JSON.stringify(attractionCache));
-                getAttractions(attractionCache);
-            }
-            else {
-                console.log('Error: ' + httpRequest.status + ' ' + httpRequest.statusText);
-                getAttractions(null);
-            }
-        }
-    };
-
-httpRequest.open("GET","java/servlets/attractionServlet");
+         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+             if (httpRequest.status === 200) {
+                 attractionCache = this.response;
+                 resp = JSON.parse(xmlhttp.responseText);
+                 localStorage.setItem('attractionCache', JSON.stringify(attractionCache));
+                 getAttractions(attractionCache);
+             }
+             else {
+                 console.log('Error: ' + httpRequest.status + ' ' + httpRequest.statusText);
+                 getAttractions(null);
+             }
+         }
+     };
+httpRequest.open("GET","/java/servlets/AttractionServlet");
 httpRequest.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 httpRequest.setRequestHeader('find',attractionCache);
 httpRequest.responseType = 'json';
 httpRequest.send();
 }
 
-function getAttractions(attractions){
+function displayAttractions(attractions){
   clearDisplay();
     const errMsg = 'Failed to load attractions';
     var mainCont = document.getElementById('managerview');
@@ -40,13 +40,31 @@ function getAttractions(attractions){
     else {
         for(i=0; i<resp.length;i++){
             var div = document.createElement('div');
-            div.innerHtml('<img scr='+resp[i].url+'><br/>'+ resp[i].name +', ID#:'+resp[i].id+'<br/> Rating:'+ resp[i].rate+'<br/>Status: '+ resp[i].status);
+            div.innerHtml('<img scr='+response[i].url+'><br/>'+ response[i].name +', ID#:'+response[i].id+'<br/> Rating:'+ response[i].rate+'<br/>Status: '+ resp[i].status);
             mainCont.appendChild(div);
         }
     }
-
-
 }
+
 function clearDisplay() {
     document.getElementById('managerview').innerHTML = '';
 }
+
+function applyFilter(attractLoc) {
+    if (!attractionCache) {
+        console.log('How did this happen?');
+        return;
+    }
+
+    if (attractLoc.length != 0) {
+        getAttractions(attractLoc);
+    }
+    else {
+        getAttractions(attractionCache);
+    }
+}
+
+function init() {
+    getAttractions();
+}
+init();
--- Coaster_manager/target/Coaster_manager/managerfront.js
+++ Coaster_manager/target/Coaster_manager/managerfront.js
@@ -1,34 +1,35 @@
 let attractionCache;
 
-function changeHTML(){
+function getAttractions(){
 const httpRequest = new XMLHttpRequest();
+
     if (!httpRequest) {
         console.log('Failed to create an XMLHttpRequest instance');
-        displayAttractions(null);
+        getAttractions(null);
         return null;
     }
  httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                attractionCache = this.response;
-                localStorage.setItem('attractionCache', JSON.stringify(attractionCache));
-                getAttractions(attractionCache);
-            }
-            else {
-                console.log('Error: ' + httpRequest.status + ' ' + httpRequest.statusText);
-                getAttractions(null);
-            }
-        }
-    };
-
-httpRequest.open("GET","attractionServlet");
+         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+             if (httpRequest.status === 200) {
+                 attractionCache = this.response;
+                 resp = JSON.parse(xmlhttp.responseText);
+                 localStorage.setItem('attractionCache', JSON.stringify(attractionCache));
+                 getAttractions(attractionCache);
+             }
+             else {
+                 console.log('Error: ' + httpRequest.status + ' ' + httpRequest.statusText);
+                 getAttractions(null);
+             }
+         }
+     };
+httpRequest.open("GET","/java/servlets/AttractionServlet");
 httpRequest.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 httpRequest.setRequestHeader('find',attractionCache);
 httpRequest.responseType = 'json';
 httpRequest.send();
 }
 
-function getAttractions(attractions){
+function displayAttractions(attractions){
   clearDisplay();
     const errMsg = 'Failed to load attractions';
     var mainCont = document.getElementById('managerview');
@@ -37,34 +38,33 @@ function getAttractions(attractions){
         mainCont.innerText = errMsg;
     }
     else {
-
         for(i=0; i<resp.length;i++){
             var div = document.createElement('div');
-            div.innerHtml('<img scr='+resp[i].url+"><br/> "+ resp[i].name +", ID#:"+resp[i]+"<br/> Rating:"+ resp[i].rate+"<br/>Status: "+ resp[i]);
+            div.innerHtml('<img scr='+response[i].url+'><br/>'+ response[i].name +', ID#:'+response[i].id+'<br/> Rating:'+ response[i].rate+'<br/>Status: '+ resp[i].status);
             mainCont.appendChild(div);
         }
     }
-
-
 }
 
-function applyFilter(attractLoc){
-if (!productCache) {
+function clearDisplay() {
+    document.getElementById('managerview').innerHTML = '';
+}
+
+function applyFilter(attractLoc) {
+    if (!attractionCache) {
         console.log('How did this happen?');
         return;
     }
 
     if (attractLoc.length != 0) {
-        let filtered = attractionCache.attractions.filter(p => {
-            return a.attractLoc === attractLoc;
-        });
-
-        getAttractions({"find": filtered});
+        getAttractions(attractLoc);
     }
     else {
         getAttractions(attractionCache);
     }
 }
 
-
+function init() {
+    getAttractions();
 }
+init();

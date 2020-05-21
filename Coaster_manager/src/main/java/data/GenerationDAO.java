949e55b1ddd1736fb682e7c590dc0b744a76ce68
Jean Aldoph II <danielaldoph@yahoo.com>
2020-05-20 21:08:24 -0400

    75% of generation done

--- Coaster_manager/src/main/java/data/GenerationDAO.java
+++ Coaster_manager/src/main/java/data/GenerationDAO.java
@@ -1,12 +1,13 @@
 package data;
 
+import com.google.gson.Gson;
 import models.Attraction;
+import models.Customer;
+import models.Employee;
 import utils.PostgresConnectionUtil;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
@@ -16,10 +17,8 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.params.HttpClientParams;
 import org.apache.commons.httpclient.params.HttpMethodParams;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
+
+import java.util.*;
 
 
 public class GenerationDAO {
@@ -30,14 +29,15 @@ public class GenerationDAO {
 
 
     public void makeAday() throws IOException {
-        FileReader in = null;
+        FileReader in = new FileReader("resources\\ticketsSoldLAst.txt");
+        BufferedReader br = new BufferedReader(in);
         Integer oldTickets = 0;
         try
         {
 
-            in = new FileReader("resources/ticketsSoldLAst.txt");
-            System.out.println(in.read());
-            oldTickets = new Integer(in.read());
+            oldTickets = new Integer(br.readLine());
+            System.out.println(oldTickets);
+
 
         }
         catch (Exception e)
@@ -53,41 +53,50 @@ public class GenerationDAO {
         Integer base, ticketsSold;
         Integer move;
         Integer ticketDiff;
-
         base = 0;
         List<Attraction> attractions = new ArrayList<Attraction>();
         Integer ticketsNow;
         Attraction temp;
         ticketsSold = 0;
         Integer ticketRating = 0;
+        SQLDatabaseIntAttraction intAttractionDB = new  SQLDatabaseIntAttraction(new PostgresConnectionUtil());
         SQLDatabaseExtAttractions extAttractionDB = new  SQLDatabaseExtAttractions(new PostgresConnectionUtil());
-        Integer iterationBound = 0;
+        SQLDatabaseCustomerDAO customerDB = new SQLDatabaseCustomerDAO(new PostgresConnectionUtil());
+        SQLDatabaseEmployees employeeDB = new SQLDatabaseEmployees(new PostgresConnectionUtil());
+        Attraction attraction;
+        Customer customer;
+        Employee employee;
+        List<Customer> list = customerDB.findAll();
+        Integer iterationBound = 500;
         while(base++ < iterationBound)
         {
+            //System.out.println(base);
             move = rand.nextInt(10_000);
             if((move < 10_000) & (move > 6_333))
             {
                 continue;
             }
-            else if ((move < 6_333)& (move > 3_333)) //New Customer buys 1-9 tickets
+            else if ((move < 6_333)& (move > 3_333)) //Old Customer buys 1-9 tickets
             {
-                ticketsNow = rand.nextInt(9);
-                //Manager findall Customers to make sure new customer isn't in DB
-                //send message to customerTickets to make customer tickets
-                //ticketsNow # of tickets for Tickets sent for customer
-                //
+                ticketsNow = rand.nextInt(10);
+                //select random sutomer form list
+                customer = list.get(rand.nextInt(list.size()));
+                //send message to customerTickets to make customer tickets.;
+                ticketsSold += ticketsNow;
             }
             else if (move < 3_333)
             {
-                ticketsNow = rand.nextInt(9);
+                ticketsNow = rand.nextInt(10);
                 //Manager findbyID Customers to make sure new customer is in DB
+                //Customer temporaryCustomer = makeCustomer();
+                //customerDB.findById(temporaryCustomer.getCustomerID());
                 //Send message to Coaster customer to add these tickets for new customer
-                //ticketsSold += ticketNow # of tickets for Tickets sent for customer
-                //
+                ticketsSold += ticketsNow; //# of tickets for Tickets sent for customer
+
             }
         }
         //read from file how many tickets were made last time
-        //oldTickets = new Integer(fileio)
+
         ticketDiff = oldTickets - ticketsSold;
         if ((ticketDiff > 89 ) || (ticketDiff<-89)) ticketRating = 10;
         else if ((ticketDiff > 69 ) || (ticketDiff<-69)) ticketRating = 9;
@@ -96,10 +105,11 @@ public class GenerationDAO {
         else if ((ticketDiff > 29 ) || (ticketDiff<-29)) ticketRating = 6;
         else if ((ticketDiff > 19 ) || (ticketDiff<-19)) ticketRating = 5;
         else ticketRating = 4;
-
+        System.out.println(ticketRating);
 
         if(ticketDiff > 0)
         {
+            int wait = 0;
             attractions = extAttractionDB.findAll();
             Iterator it = attractions.iterator();
             while (it.hasNext())
@@ -107,11 +117,69 @@ public class GenerationDAO {
                 temp = (Attraction) it.next();
                 if (temp.getRating()==ticketRating)
                 {
-                    System.out.println(temp);
+                    //System.out.println(temp);
+
+                    wait++;
                 }
             }
+            System.out.println(wait);
             }
+            FileWriter win = new FileWriter("resources\\ticketsSoldLAst.txt");
 
+            try
+            {
+                PrintWriter write = new PrintWriter(win);
+                write.println(ticketsSold);
+                write.close();
+                System.out.println(ticketDiff);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (in != null) in.close();
 
+            }
     }
+
+//    private Customer makeCustomer() {
+//        return new Customer();
+//    }
+
+    public Employee makeEmployee()
+    {
+        
+    }
+
+    public String indicativeFoul()
+    {
+        String url = "https://randomuser.me/api/";
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                new DefaultHttpMethodRetryHandler(3, false));
+
+        try {
+            int statusCode = client.executeMethod(method);
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed: " + method.getStatusLine());
+            }
+            byte[] responseBody = method.getResponseBody();
+            System.out.println(new String(responseBody));
+            responseBody.
+        } catch (HttpException e) {
+            System.err.println("Fatal protocol violation: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Fatal transport error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+
+        }
+    }
+
 }
--- Coaster_manager/src/test/java/GenerationTests.java
+++ Coaster_manager/src/test/java/GenerationTests.java
@@ -10,7 +10,16 @@
 import data.GenerationDAO;
 import org.junit.Test;
 import utils.PostgresConnectionUtil;
-
+import java.io.IOException;
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpClientParams;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -29,19 +38,75 @@ public class GenerationTests {
 // //call function from script file
 // inv.invokeFunction("yourFunction", "param");
 
-@Test
-public void testGen()
-{
-
-    GenerationDAO genDao = new GenerationDAO();
-    try
+    @Test
+    public void testGen()
     {
-        genDao.makeAday();
-    }
-    catch (Exception e)
-    {
-        e.printStackTrace();
-    }
-}
 
+        GenerationDAO genDao = new GenerationDAO();
+        try
+        {
+            genDao.makeAday();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+//    Article Body
+//
+//    Before implementing a call, make sure you have the URL for the API call, a valid API Key for the environment you are connecting to,
+//    and any required data elements. In this example, it is requesting for details of an order by the Transaction ID.
+//
+//        Import a HTTP Client.
+//        Assign the URL and get params to the URL string.
+//        Setup the http client.
+//        Put it in GET mode.
+//        Assign the URL and parameters.
+//        Make the request.
+//        Process the Response.
+//        Close the connection.
+//
+//
+    @Test
+    public void testCall()
+    {
+        // Fake example transaction ID: 3YC00XQKNVMZ
+        String url = "https://randomuser.me/api/";
+
+        // Create an instance of HttpClient.
+        HttpClient client = new HttpClient();
+
+        // Create a method instance.
+        GetMethod method = new GetMethod(url);
+
+        // Provide custom retry handler is necessary
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                new DefaultHttpMethodRetryHandler(3, false));
+
+        try {
+            // Execute the method.
+            int statusCode = client.executeMethod(method);
+
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed: " + method.getStatusLine());
+            }
+
+
+            // Read the response body.
+            byte[] responseBody = method.getResponseBody();
+            System.out.println(new String(responseBody));
+
+        } catch (HttpException e) {
+            System.err.println("Fatal protocol violation: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Fatal transport error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+        }
+    }
 }

d70279602ac951b6c6fd4e49c24aab7906e66e2e
rjefferson <reginald.jefferson@outlook.com>
2020-05-22 17:07:42 -0400

    added redirects. hopefully this works lol

--- Coaster_manager/src/main/webapp/managerlogin.js
+++ Coaster_manager/src/main/webapp/managerlogin.js
@@ -1,21 +1,53 @@
-function login() {
-    let data = {
-        empID: document.getElementById("empId").value,
-        pword: document.getElementById("password").value,
-        add: 'login'
-    }
-    $.ajax({
-        url: '/employeeServlet',
-        type: 'POST',
-        dataType: 'json',
-        data: JSON.stringify(data),
-        contentType: 'application/json',
-        mimeType: 'application/json',
-        success: function (response) {
-
+document.getElementById('login').addEventListener('click', (e) => {
+    e.preventDefault;
+    let email = document.getElementById("email").value;
+    let pword = document.getElementById("password").value;
+    if(email.length != 0 && pword.length != 0){
+        let data = {
+            email: document.getElementById("email").value,
+            pword: document.getElementById("password").value,
+            add: 'login'
         }
-    })
-}
+        $.ajax({
+            url: 'employeeServlet',
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            mimeType: 'application/json',
+            success: function (response) {
+                // alert(response);
+                if (response.adminPriv == "true") {
+                    document.cookie = email;
+                    window.location.replace("http://www.bing.com")
+                }
+                else {
+                    window.location.href="login.html"
+                }
+            }
+        })
+    }
+})
+
+// function login() {
+//     let data = {
+//         empID: document.getElementById("empId").value,
+//         pword: document.getElementById("password").value,
+//         add: 'login'
+//     }
+//     $.ajax({
+//         url: '/employeeServlet',
+//         type: 'POST',
+//         dataType: 'json',
+//         data: JSON.stringify(data),
+//         contentType: 'application/json',
+//         mimeType: 'application/json',
+//         success: function (response) {
+//
+//         }
+//     })
+// }
+
 // $(document).ready(function() {
 //     $("#login").click(function () {
 //         let data = {

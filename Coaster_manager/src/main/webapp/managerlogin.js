5845d2cc997e27dc716f7840ded5794373e05fb0
rjefferson <reginald.jefferson@outlook.com>
2020-05-22 18:11:07 -0400

    login working finally with assistance form Joshua

--- Coaster_manager/src/main/webapp/managerlogin.js
+++ Coaster_manager/src/main/webapp/managerlogin.js
@@ -1,33 +1,57 @@
-document.getElementById('login').addEventListener('click', (e) => {
-    e.preventDefault;
-    let email = document.getElementById("email").value;
-    let pword = document.getElementById("password").value;
-    if(email.length != 0 && pword.length != 0){
-        let data = {
-            email: document.getElementById("email").value,
-            pword: document.getElementById("password").value,
-            add: 'login'
-        }
-        $.ajax({
-            url: 'employeeServlet',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            contentType: 'application/json',
-            mimeType: 'application/json',
-            success: function (response) {
-                // alert(response);
-                if (response.adminPriv == "true") {
-                    document.cookie = email;
-                    window.location.replace("http://www.bing.com")
-                }
-                else {
-                    window.location.href="login.html"
-                }
-            }
-        })
+function loginCall() {
+    let data = {
+        id: document.getElementById("empId").value, //changed from empID to id
+        pword: document.getElementById("password").value,
+        add: 'login'
     }
-})
+    $.ajax({
+        url: 'employeeServlet',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        mimeType: 'application/json',
+        success: function (response) {
+            if(response.adminPriv === 'false'){ //parsing response and doing a task
+                window.location.href = 'index.html';
+            } else{
+                window.location.href = 'login.html';
+            }
+        }
+    })
+}
+
+// document.getElementById('login').addEventListener('click', (e) => {
+//     e.preventDefault;
+//     let email = document.getElementById("email").value;
+//     let pword = document.getElementById("password").value;
+//     if(email.length != 0 && pword.length != 0){
+//         let data = {
+//             email: document.getElementById("email").value,
+//             pword: document.getElementById("password").value,
+//             add: 'login'
+//         }
+//         $.ajax({
+//             url: 'employeeServlet',
+//             type: 'POST',
+//             dataType: 'json',
+//             data: JSON.stringify(data),
+//             contentType: 'application/json',
+//             mimeType: 'application/json',
+//             success: function (response) {
+//                 // alert(response);
+//                 if (response.adminPriv == "true") {
+//                     document.cookie = email;
+//                     window.location.replace("http://www.bing.com");
+//                 }
+//                 else {
+//                     window.location.href="login.html"
+//                 }
+//             }
+//         })
+//     }
+// })
+
 
 // function login() {
 //     let data = {

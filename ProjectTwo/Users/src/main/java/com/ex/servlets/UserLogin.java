ead77d55eba58638fe5c34e3cb966847a68cb4a1
Daniel Wallace <walldiv@gmail.com>
2020-05-25 13:26:28 -0400

    Fixed cookie error in UserLogin servlet

--- ProjectTwo/Users/src/main/java/com/ex/servlets/UserLogin.java
+++ ProjectTwo/Users/src/main/java/com/ex/servlets/UserLogin.java
@@ -46,12 +46,15 @@ public class UserLogin extends HttpServlet {
         String json = objectMapper.writeValueAsString(user);
 
         //cookies for the client side rendering
-        Cookie c1 = new Cookie("userName", user.getFirstname() + " " + user.getLastname());
+        Cookie c1 = new Cookie("userName", user.getFirstname());
         c1.setMaxAge(60*60);
+        Cookie c2 = new Cookie("userEmail", user.getEmail());
+        c2.setMaxAge(60*60);
 
         resp.setContentType("application/json");
         resp.setStatus(200);
         resp.addCookie(c1);
+        resp.addCookie(c2);
         resp.getWriter().write(json);
         System.out.println("UserLogin::COOKIE - " + c1.toString());
 

cf60700065011dfd0ddc7420836c66ae92aaa3e3
Perry Lee <perrylee_2004@yahoo.com>
2020-05-26 00:20:19 -0400

    Auto-set seller ID when creating auction

--- Auction/src/main/java/auction/web/AuctionServlet.java
+++ Auction/src/main/java/auction/web/AuctionServlet.java
@@ -29,7 +29,7 @@ public class AuctionServlet extends HttpServlet {
     private AuctionService service;
     private AuctionJSONService jsonService;
     private AuctionJSONConverter jsonConverter;
-    UserDAO userDao;
+    private UserDAO userDao;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -39,8 +39,17 @@ public class AuctionServlet extends HttpServlet {
         service = (AuctionService)context.getAttribute("auctionService");
         jsonService = (AuctionJSONService)context.getAttribute("jsonService");
         jsonConverter = (AuctionJSONConverter)context.getAttribute("jsonConverter");
+        userDao = (UserDAO)context.getAttribute("userDao");
     }
 
+    private User getUser(Cookie[] cookies) {
+        for (Cookie cookie: cookies) {
+            if (cookie.getName().equals("userName")) {
+                return userDao.findByUserName(cookie.getValue());
+            }
+        }
+        return null;
+    }
     private String getRequestBody(HttpServletRequest req) throws IOException {
         return req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
     }
@@ -146,7 +155,12 @@ public class AuctionServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO make sure that user is authenticated and has the proper permissions to do this
+        User seller = getUser(req.getCookies());
+        // Don't allow user to create an auction if they're not logged in or if they're banned
+        if (seller == null || seller.getRole() == 3) {
+            resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            return;
+        }
         String url = req.getPathInfo();
         boolean success = false;
         if (url == null || url.equals("")) {
@@ -154,6 +168,7 @@ public class AuctionServlet extends HttpServlet {
             AuctionJSONWrapper newAuction = jsonConverter.deserializeAuction(json);
             if (newAuction != null) {
                 Auction auction = newAuction.toAuction();
+                auction.setSellerID(seller.getUserId());
                 Item auctionItem = newAuction.toItem();
                 Auction ret = service.createAuction(auction, auctionItem);
                 if (ret != null) {
@@ -204,41 +219,22 @@ public class AuctionServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO make sure that user is authenticated and has the proper permissions to do this
+        User user = getUser(req.getCookies());
+        if (user == null || user.getRole() != 2) { // Only admins can delete auctions
+            resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            return;
+        }
 
         String url = req.getPathInfo();
-        try {
-
-            PrintWriter out = resp.getWriter();
-            Cookie[] cookies = req.getCookies();
-            if (cookies != null) {
-                for (int i = 0; i < cookies.length; i++) {
-                    if (cookies[i].getName().equals("userName")) {
-                        User newUser = userDao.findByUserName(cookies[i].getValue());
-                        if (newUser.getRole() == 2 ) {
-
-                            if (url == null || url.equals(""))
-                                resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-                            else {
-                                int id = getID(url);
-                                if (id >= 0) {
-                                    service.removeAuction(id);
-                                    resp.setStatus(HttpServletResponse.SC_OK);
-                                } else
-                                    resp.sendError(-id);
-                            }
-                        } else
-                            resp.setStatus(201);
-                            out.write("Don't have valid access");
-
-                    }
-                }
-            }
-        } catch(Exception e)
-        {
-            resp.setStatus(206);
-            PrintWriter out = resp.getWriter();
-            out.write("Something Went Wrong");
+        if (url == null || url.equals(""))
+            resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        else {
+            int id = getID(url);
+            if (id >= 0) {
+                service.removeAuction(id);
+                resp.setStatus(HttpServletResponse.SC_OK);
+            } else
+                resp.sendError(-id);
         }
     }
 
